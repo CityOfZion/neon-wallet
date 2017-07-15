40d5aab0f98e782b8d60e183207cb7a49d3519d8
Nick Emmons <emmons.ventures@gmail.com>
2017-07-15 03:51:13 -0400

    balance buttons to icons

--- app/components/Balance.js
+++ app/components/Balance.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router'
 import { getBalance } from '../wallet/api.js';
 import { setBalance } from '../actions/index.js';
+var MdRefresh = require('react-icons/lib/md/refresh');
+var MdSend = require('react-icons/lib/md/send');
+var MdLogout = require('react-icons/lib/md/exit-to-app');
 
 const initiateGetBalance = (dispatch, net, address) => {
   getBalance(net, address).then(function(result){
@@ -34,11 +37,11 @@ class Balance extends Component {
         <div><span className="asset">NEO: </span><span className="amount">{ this.props.ans }</span></div>
         <div><span className="asset">Gas: </span><span className="amount">{ this.props.anc }</span></div>
       </div>
-      <div className="margin10">
-        <button onClick={() => initiateGetBalance(this.props.dispatch, this.props.net, this.props.address)}>Refresh</button>
-        <button><Link to="/send">Send Transaction</Link></button>
+      <div className="topRight">
+        <MdRefresh onClick={() => initiateGetBalance(this.props.dispatch, this.props.net, this.props.address)} className="customIcon" />
+        <Link to="/send" ><MdSend className="customIcon"/></Link>
+        <Link to="/"><MdLogout className="customIcon"/></Link>
       </div>
-      <button><Link to="/">Logout</Link></button>
     </div>;
 }
 
