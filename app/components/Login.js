f0da64299ea50fba09bee28e53852b76b25d04bc
Fabio C. Canesin <fabio.canesin@gmail.com>
2017-07-28 11:27:38 -0400

    Add Hex login

--- app/components/Login.js
+++ app/components/Login.js
@@ -4,21 +4,23 @@ import { login, setBalance, resetPrice } from '../actions/index.js';
 import { Link } from 'react-router';
 import CreateWallet from './CreateWallet.js'
 import { getBlockByIndex } from '../wallet/api.js';
+import { getWIFFromHexPrivateKey } from '../wallet/index.js';
 
 let input_wif;
 
 const onWifChange = (dispatch) => {
   // lookup wif address to check whether it is valid and enable login
-  dispatch(login(input_wif.value));
-  // somewhat hacky way to reset price on every login
-  // dispatch(resetPrice());
-
+  if (input_wif.value.length > 51) {
+    dispatch(login(getWIFFromHexPrivateKey(input_wif.value)));  
+  } else {
+    dispatch(login(input_wif.value));  
+  }
 };
 
 let Login = ({ dispatch, loggedIn, wif }) =>
   <div id="loginPage">
     <div className="login">
-      <input type="text" placeholder="Enter your private key here (WIF)" onChange={() => onWifChange(dispatch)} ref={node => {input_wif = node;}} />
+      <input type="text" placeholder="Enter your private key here (WIF or Hex)" onChange={() => onWifChange(dispatch)} ref={node => {input_wif = node;}} />
       <div className="loginButtons">
         {loggedIn ? <button><Link to="/dashboard">Login</Link></button> : <button disabled="true">Login</button>}
         <button><Link to="/create">New Wallet</Link></button>
