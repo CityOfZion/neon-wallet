9c3e223029d8f125e169c14affc6e3244fa51adf
Alessio <alessio.d@gmail.com>
2017-08-10 14:07:34 +0200

    Adding the ExplorerSwitch component + key on TransactionHistory li

---
+++ app/components/ExplorerSwitch.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setExplorer } from '../modules/explorer';
+
+class ExplorerSwitch extends Component {
+  componentDidMount = () => {
+    console.log("state", this.state);
+    console.log("props", this.props);
+    this.toggleExplorer = this.toggleExplorer.bind(this);
+  }
+  
+  toggleExplorer(){
+    const explorer = this.props.explorer.label === 'Antcha' ? 'NeoTracker' : 'Antcha';
+    this.props.dispatch( setExplorer(explorer) );
+  }
+
+
+  render = () =>
+    <div id="explorerSwitch">
+      <span className="transparent">Explorer: </span>
+      <span className="explorerName" onClick={() => this.toggleExplorer()}>{this.props.explorer.label}</span>
+    </div>;
+
+}
+
+const mapStateToProps = (state) => ({
+  explorer: state.explorer.selected
+});
+
+ExplorerSwitch = connect(mapStateToProps)(ExplorerSwitch);
+
+export default ExplorerSwitch;
