41ddee30c0de9440525d6e21626c6085704d0cc2
Ethan Fast <Ejhfast@gmail.com>
2017-08-31 00:03:35 +1000

    connect settings to redux

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { setBlockExplorer } from '../modules/metadata';
 
 const logo = require('../images/neon-logo2.png');
 
-const Settings = ({}) =>
+let explorer_select;
+
+let Settings = ({dispatch, explorer}) =>
   <div id="settings">
     <div className="logo"><img src={logo} width="60px"/></div>
     <div className="title">Settings:</div>
     <div className="settingsForm">
       <div className="settingsItem">
         <label>Block Explorer:</label>
-        <select>
+        <select value={explorer} ref={(node) => explorer_select = node} onChange={() => dispatch(setBlockExplorer(explorer_select.value))}>
           <option>Neotracker</option>
           <option>Antchain</option>
         </select>
@@ -18,5 +22,11 @@ const Settings = ({}) =>
     </div>
     <Link to="/"><button className="altButton">Home</button></Link>
   </div>
+
+const mapStateToProps = (state) => ({
+  explorer: state.metadata.blockExplorer,
+});
+
+Settings = connect(mapStateToProps)(Settings);
 
 export default Settings;
