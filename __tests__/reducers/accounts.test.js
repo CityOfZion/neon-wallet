321d7b9c240c94ef4d70fb99ba7a71da3a10a838
Aitor Fernández <helloimaitor@gmail.com>
2017-09-28 12:45:25 +0100

    Unit test for account.js and claimState.js reducers (#54)
    Unit test for account.js and claimState.js reducers

---
+++ __tests__/reducers/accounts.test.js
@@ -0,0 +1,51 @@
+import * as types from '../../app/actions/types';
+import account from '../../app/reducers/account';
+
+describe('accounts reducer', () => {
+  let state = { wif: null, address: null, loggedIn: false };
+  let action;
+
+  beforeEach(() => {
+    action = {
+      wif: 'L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A',
+      type: types.LOGIN
+    }
+  });
+
+  describe('LOGIN', () => {
+    test('with invalid loadAccount', () => {
+      action.wif = null;
+      let received = account(state, action);
+      let expected = {
+        wif: null,
+        address: null,
+        loggedIn: false
+      };
+
+      expect(received).toEqual(expected);
+    });
+
+    test('with valid loadAccount', () => {
+      let received = account(state, action);
+      let expected = {
+        wif: action.wif,
+        address: 'AMg5pC6PryhWoWWbcETq3kt4GC5Pj1i5kQ',
+        loggedIn: true
+      };
+
+      expect(received).toEqual(expected);
+    });
+  });
+
+  test('LOGOUT', () => {
+    action.type = types.LOGOUT;
+    let received = account(state, action);
+    let expected = {
+      wif: null,
+      address: null,
+      loggedIn: false
+    };
+
+    expect(received).toEqual(expected);
+  });
+});
