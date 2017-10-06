6b244da629506d99fba8ea642a59fe1195fb1032
Kevin Ko <enhasa@gmail.com>
2017-10-06 11:50:10 -0700

    Use input type password for login field (#69)
    * Use input type password for login field
    
    * WIP: Add toggle to show/hide private key at login
    
    * Propagate state in showKey return
    
    * Add showkey login toggle test
    
    * Change Login to react component to handle component-level state changes
    
    * Apply visibility toggle to sensitive login fields
    
    * Remove redundant footer styling (merge conflict redundancy)

--- app/components/LoginPrivateKey.js
+++ app/components/LoginPrivateKey.js
@@ -6,56 +6,106 @@ import { getWIFFromPrivateKey } from 'neon-js';
 import { encrypt_wif, decrypt_wif } from 'neon-js';
 import { sendEvent, clearTransactionEvent } from '../modules/transactions';
 import { getAccountsFromWIFKey } from 'neon-js';
+import FaEye from 'react-icons/lib/fa/eye';
+import FaEyeSlash from 'react-icons/lib/fa/eye-slash';
 
-let wif;
+class LoginPrivateKey extends React.Component {
+  constructor(props) {
+    super(props);
 
-const logo = require('../images/neon-logo2.png');
+    this.state = {
+      showKey: false,
+      wif: '',
+    };
 
-// TODO: move to neon-js
-const verifyPrivateKey = (wif) => {
-  try {
-    // TODO: better check
-    getAccountsFromWIFKey(wif)[0].address;
+    this.toggleKeyVisibility = this.toggleKeyVisibility.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  catch (e){
-    return false;
-  }
-  return true;
-};
 
-const onWifChange = (dispatch, history, wif) => {
-  const value = wif.value;
-  // TODO: changed back to only WIF login for now, getting weird errors with private key hex login
-  if (verifyPrivateKey(value) === true){
-    dispatch(login(value));
-    history.push('/dashboard');
-  }
-  else {
-    dispatch(sendEvent(false, "That is not a valid private key"));
-    setTimeout(() => dispatch(clearTransactionEvent()), 5000);
-  }
-};
+  static propTypes = {
+    loggedIn: React.PropTypes.bool.isRequired,
+    wif: React.PropTypes.string,
+    onWifChange: React.PropTypes.func.isRequired,
+    verifyPrivateKey: React.PropTypes.func.isRequired,
+  };
 
-let LoginPrivateKey = ({ dispatch, loggedIn, wif, history }) =>
-  <div id="loginPage">
-    <div className="login">
-      <div className="loginForm">
-        <div className="logo"><img src={logo} width="60px"/></div>
-        <input type="text" placeholder="Enter your private key here (WIF)" ref={(node) => wif = node}/>
+  toggleKeyVisibility = () => {
+    this.setState(prevState => ({
+      showKey: !prevState.showKey,
+    }));
+  };
+
+  handleInputChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({
+      wif: value,
+    });
+  }
+
+  handleVerify = () => {
+    this.props.onWifChange(this.props.dispatch, this.props.verifyPrivateKey, this.props.history, this.state.wif);
+  }
+
+  render = () => {
+    let { loggedIn, wif } = this.props;
+    const { showKey } = this.state;
+    const logo = require('../images/neon-logo2.png');
+
+    return (
+      <div id="loginPage">
+        <div className="login">
+          <div className="loginForm">
+            <div className="logo"><img src={logo} width="60px"/></div>
+            <input type={showKey ? 'text' : 'password'} placeholder="Enter your private key here (WIF)" onChange={this.handleInputChange} />
+
+            {showKey ?
+              <FaEyeSlash className="viewKey" onClick={this.toggleKeyVisibility} /> :
+              <FaEye className="viewKey" onClick={this.toggleKeyVisibility} />
+            }
+          </div>
+          <div className="loginButtons">
+            <button onClick={this.handleVerify}>Login</button>
+            <Link to="/"><button className="altButton">Home</button></Link>
+          </div>
+          <div id="footer">Created by Ethan Fast and COZ. Donations: Adr3XjZ5QDzVJrWvzmsTTchpLRRGSzgS5A</div>
+        </div>
       </div>
-      <div className="loginButtons">
-        <button onClick={(e) => onWifChange(dispatch, history, wif)}>Login</button>
-        <Link to="/"><button className="altButton">Home</button></Link>
-      </div>
-      <div id="footer">Created by Ethan Fast and COZ. Donations: Adr3XjZ5QDzVJrWvzmsTTchpLRRGSzgS5A</div>
-    </div>
-  </div>;
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   loggedIn: state.account.loggedIn,
-  wif: state.account.wif
+  wif: state.account.wif,
 });
 
-LoginPrivateKey = connect(mapStateToProps)(LoginPrivateKey);
+const mapActionCreators = (dispatch) => {
+  return {
+    dispatch,
+    // TODO: move to neon-js
+    verifyPrivateKey: (wif) => {
+      try {
+        // TODO: better check
+        getAccountsFromWIFKey(wif)[0].address;
+      }
+      catch (e){
+        return false;
+      }
+      return true;
+    },
+    onWifChange: (dispatch, verifyPrivateKey, history, wif) => {
+      // TODO: changed back to only WIF login for now, getting weird errors with private key hex login
+      if (verifyPrivateKey(wif) === true){
+        dispatch(login(wif));
+        history.push('/dashboard');
+      }
+      else {
+        dispatch(sendEvent(false, "That is not a valid private key"));
+        setTimeout(() => dispatch(clearTransactionEvent()), 5000);
+      }
+    }
+  }
+};
 
-export default LoginPrivateKey;
+export default connect(mapStateToProps, mapActionCreators)(LoginPrivateKey);
