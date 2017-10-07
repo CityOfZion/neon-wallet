31fc6f0e0f9adce4e7e8319ae7d0bc8c801ce0d4
Ethan Fast <Ejhfast@gmail.com>
2017-10-07 13:07:04 -0700

    updates for sale

--- app/components/TokenSale.js
+++ app/components/TokenSale.js
@@ -36,13 +36,13 @@ const refreshTokenBalance = (dispatch, net, address, silent = false) => {
 
 const participateInSale = (dispatch, net, wif, totalNeo) => {
   const account = Neon.getAccountFromWIFKey(wif);
-  const toMint = parseInt(neoToSend.value);
-  neoToSend.value = "";
-  if (parseFloat(toMint) !== parseInt(toMint)){
+  if (parseFloat(neoToSend.value) !== parseInt(neoToSend.value)){
     dispatch(sendEvent(false, "You cannot send fractional Neo to a token sale."));
     setTimeout(() => dispatch(clearTransactionEvent()), 5000);
     return false;
   }
+  const toMint = parseInt(neoToSend.value);
+  neoToSend.value = "";
   if (toMint > totalNeo){
     dispatch(sendEvent(false, "You do not have enough Neo to send."));
     setTimeout(() => dispatch(clearTransactionEvent()), 5000);
@@ -59,7 +59,8 @@ const participateInSale = (dispatch, net, wif, totalNeo) => {
       dispatch(sendEvent(true, "Processing..."));
       setTimeout(() => dispatch(clearTransactionEvent()), 5000);
       Neon.doMintTokens(net, scriptHash, wif, toMint, 0).then((response) => {
-        console.log(response)
+        dispatch(sendEvent(true, "Processing..."));
+        setTimeout(() => dispatch(clearTransactionEvent()), 5000);
         if(response.result === true){
           dispatch(sendEvent(true, "Sale participation was successful."));
           setTimeout(() => dispatch(clearTransactionEvent()), 5000);
@@ -88,10 +89,12 @@ class TokenSale extends Component {
   componentDidMount = () => {
     this.props.dispatch(updateRpxBalance(0))
     initiateGetBalance(this.props.dispatch, this.props.net, this.props.address)
+    refreshTokenBalance(this.props.dispatch, this.props.net, this.props.address, true)
+
   }
 
   componentDidUpdate = () => {
-    refreshTokenBalance(this.props.dispatch, this.props.net, this.props.address, true)
+    // refreshTokenBalance(this.props.dispatch, this.props.net, this.props.address, true)
   }
 
   render = () =>
@@ -102,8 +105,9 @@ class TokenSale extends Component {
       <div className="warning">
         <b>WARNING:</b> Be very careful with how you participate in a sale! This interface may not work for all sales! Submitting NEO multiple times to a sale may result in lost funds
         or a delayed refund depending on the policy of the sale. CoZ is not responsible for any mistakes you make participating in
-        a sale. After submitting to a sale, you will need to <b>WAIT SOME TIME</b> for balance of token to refresh. You can click
-        "Refresh Token" after 10s if you still do not see anything. Do not click "Submit" twice. CoZ does not endorse any token sale!
+        a sale. After submitting to a sale, you will need to <b>WAIT SOME TIME</b> for the balance of tokens to refresh. You can click
+        "Refresh Token" after 10s if you still do not see anything. It is also possible that nodes may not update properly with your token balance,
+        so <b>THINK VERY CAREFULLY</b> before resubmitting to a sale. Do not click "Submit" twice. CoZ does not endorse any token sale!
       </div>
       <div className="settingsForm">
         <div className="settingsItem">
