137f1326d86c132e67a4db042a2eeebb0b42259b
phetter <fet@fet>
2017-10-12 23:44:21 -0400

    created unit test for LoginLocalStorage

---
+++ __tests__/components/LoginLocalStorage.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import configureStore from 'redux-mock-store'
+import { mount } from 'enzyme'
+
+import LoginLocalStorage from '../../app/components/LoginLocalStorage'
+import { sendEvent } from '../../app/modules/transactions'
+
+const state = {
+  showKey: false,
+  account: {
+    loggedIn: false,
+    wif: null,
+    accountKeys: ['test']
+  },
+  decrypting: false
+}
+
+describe('LoginLocalStorage', () => {
+  test('should render without crashing', () => {
+    const store = configureStore()(state)
+    const wrapper = mount(<LoginLocalStorage store={store} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('pressing enter on passphrase input field triggers sendEvent', () => {
+    const store = configureStore()(state)
+    const wrapper = mount(<LoginLocalStorage store={store} />)
+
+    const c = wrapper.find('.passPhrase')
+    // console.log('debug:'+c.debug())
+    // console.log('debug2:'+c.html())
+    c.simulate('keyDown', { key: 'Enter' })
+    expect(store.getActions()[0]).toEqual(sendEvent(false, 'Passphrase too short'))
+  })
+})
