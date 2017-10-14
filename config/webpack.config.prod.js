024a6673dece8a1ecb91aab7d9a7e26e706e8430
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-10-14 22:49:41 +1100

    Upgrade to webpack3 + adds a webpack prod config + prepares for hot reloading in Electron

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  bail: true,
+  devtool: 'source-map',
+  target: 'electron',
+  entry: [
+    'babel-polyfill',
+    path.join(__dirname, '..', 'app/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '..', 'app/dist/'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  node: {
+    __dirname: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: true,
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            compact: true
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }
+      },
+      {
+        test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+          }
+        }
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  }
+}
