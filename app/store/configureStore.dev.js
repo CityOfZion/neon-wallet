72c5858dbe487a4d40d8c9f03e193393d226b984
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-10-18 22:03:20 +1100

    Adds redux-logger (dev only)

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -2,13 +2,19 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import DevTools from '../containers/DevTools'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 export default function configureStore (initialState) {
+  const middlewares = [
+    thunk,
+    logger
+  ]
+
   const store = createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(...middlewares),
       DevTools.instrument()
     )
   )
