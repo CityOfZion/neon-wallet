dc29da4c4aa42b660063441127f037838e306772
phetter <fet@fet>
2017-10-18 21:12:50 -0400

    further fixes

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -2,10 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import DevTools from '../containers/DevTools'
 import thunk from 'redux-thunk'
-<<<<<<< HEAD
-=======
-
->>>>>>> 6ea66d59ec0cdcea15f13a2fcddac2cc0529d062
 import logger from 'redux-logger'
 
 export default function configureStore (initialState) {
@@ -18,13 +14,7 @@ export default function configureStore (initialState) {
     rootReducer,
     initialState,
     compose(
-<<<<<<< HEAD
       applyMiddleware(...middlewares),
-=======
-
-      applyMiddleware(...middlewares),
-
->>>>>>> 6ea66d59ec0cdcea15f13a2fcddac2cc0529d062
       DevTools.instrument()
     )
   )
