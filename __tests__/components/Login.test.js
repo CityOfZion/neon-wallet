8307714f74ec26b9513af62e84ceb613ddc3c02a
phetter <fet@fet>
2017-10-20 11:58:31 -0400

    testing tests

--- __tests__/components/Login.test.js
+++ __tests__/components/Login.test.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import configureStore from 'redux-mock-store'
 import { shallow, mount } from 'enzyme'
-
-import { login } from '../../app/modules/account'
-
+import thunk from 'redux-thunk'
+import { login, onWifChange } from '../../app/modules/account'
 import { sendEvent } from '../../app/modules/transactions'
 import { KEYS } from '../../app/core/constants'
-
-import Login from '../../app/containers/LoginPrivateKey/LoginPrivateKey'
+import Login from '../../app/containers/LoginPrivateKey'
 
 const setup = (state = { account: {
   loggedIn: true,
   wif: undefined
 }}) => {
-  const store = configureStore()(state)
+  const store = configureStore([thunk])(state)
+  // store.dispatch = jest.fn()
   const wrapper = shallow(<Login store={store} />)
 
   return {
@@ -26,7 +25,9 @@ const state = {
   account: {
     loggedIn: false,
     wif: null
-  }
+  },
+  wif: ''
+  // dispatch: jest.fn()
 }
 
 describe('Login', () => {
@@ -36,8 +37,8 @@ describe('Login', () => {
   })
 
   test('clicking show key icon toggles private key visibility', () => {
-    const { wrapper } = setup()
-
+    const { store, wrapper } = setup()
+    const state = store.getState()
     expect(wrapper.state('showKey')).toEqual(false)
 
     wrapper
@@ -47,32 +48,42 @@ describe('Login', () => {
     expect(wrapper.state('showKey')).toEqual(true)
   })
 
-  // test('pressing enter on passphrase input field triggers sendEvent', () => {
-  //   const store = configureStore()(state)
-  //   const wrapper = mount(<Login store={store} />)
-  //   const c = wrapper.find('.passInput')
-  //
-  //   // console.log('debug:'+c.debug())
-  //   // console.log('debug2:'+c.html())
-  //   c.simulate('keyDown', { key: KEYS.ENTER })
-  //   expect(store.getActions()[0]).toEqual(sendEvent(false, 'That is not a valid private key'))
-  // })
-  //
-  // test('private key field input onChange dispatches LOGIN action', () => {
-  //   const history = {
-  //     push: jest.fn()
-  //   }
-  //
-  //   const store = configureStore()(state)
-  //   const wrapper = mount(<Login store={store} history={history} />)
-  //   const c = wrapper.find('input')
-  //
-  //   c.instance().value = 'L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A'
-  //   c.simulate('change')
-  //   wrapper.find('.loginButton').simulate('click')
-  //
-  //   expect(store.getActions()[0]).toEqual(login('L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A'))
-  // })
+  test('pressing enter on passphrase input field triggers sendEvent', () => {
+    const store = configureStore()(state)
+    const dispatch = jest.fn()
+    const wrapper = mount(<Login store={store} />)
+    const c = wrapper.find('.passInput')
+
+    // console.log('debug:'+c.debug())
+    // console.log('debug2:'+c.html())
+    c.simulate('keyDown', { key: KEYS.ENTER })
+    expect(store.getActions()[0]).toEqual(sendEvent(false, 'That is not a valid private key'))
+  })
+
+  test('private key field input onChange dispatches LOGIN action', () => {
+    const history = {
+      push: jest.fn()
+    }
+
+    const store = configureStore()(state)
+
+    const wrapper = mount(<Login store={store} history={history} onWifChange={onWifChange}  />)
+    const c = wrapper.find('.passInput')
+
+    c.instance().value = 'L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A'
+    c.simulate('change')
+    wrapper.find('.loginButton').simulate('click')
+
+    const instance = wrapper.instance()
+    const spy = jest.spyOn(instance, 'dispatch')
+
+      instance.forceUpdate();
+    // expect(c.find('.passInput').prop('value')).toEqual('L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A')
+
+    // expect(store.getActions()[0]).toEqual(login('L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A'))
+    // expect(store.getActions()[0]).toEqual(login('L1xpshXfzF6iQTq42onA5km8qwyzBaNQzPADhfTt2jzzcQSVoP5A'))
+    expect(store.dispatch).toHaveBeenCalled()
+  })
 
   // test('private key field input onChange dispatches LOGIN action', () => {
   //   const { store, wrapper } = setup();
