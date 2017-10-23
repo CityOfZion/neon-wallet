1448533c97d5e5b652348bf7d6edb6375a44a42b
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-10-23 21:37:05 +1100

    Remove unused props

--- app/containers/LoginNep2/index.js
+++ app/containers/LoginNep2/index.js
@@ -4,14 +4,10 @@ import { bindActionCreators } from 'redux'
 import LoginNep2 from './LoginNep2'
 import { loginNep2 } from '../../modules/account'
 
-const mapStateToProps = (state: Object) => ({
-  accountKeys: state.account.accountKeys
-})
-
 const actionCreators = {
   loginNep2
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginNep2)
+export default connect(null, mapDispatchToProps)(LoginNep2)
--- app/containers/LoginTokenSale/index.js
+++ app/containers/LoginTokenSale/index.js
@@ -4,14 +4,10 @@ import { bindActionCreators } from 'redux'
 import LoginTokenSale from './LoginTokenSale'
 import { loginWithPrivateKey } from '../../modules/account'
 
-const mapStateToProps = (state: Object) => ({
-  wif: state.account.wif
-})
-
 const actionCreators = {
   loginWithPrivateKey
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginTokenSale)
+export default connect(null, mapDispatchToProps)(LoginTokenSale)
