d13ec58f971aa83ee902ff3e18437b097a9fa704
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-10-30 21:05:47 +1100

    Rename to .jsx

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -9,6 +9,7 @@ const Home = () =>
     <Link to={ROUTES.LOGIN_LOCAL_STORAGE}><div className='linkBox'>Login using a saved wallet</div></Link>
     <Link to={ROUTES.LOGIN_NEP2}><div className='linkBox'>Login using an encrypted key</div></Link>
     <Link to={ROUTES.LOGIN_PRIVATE_KEY}><div className='linkBox'>Login using a private key</div></Link>
+    <Link to={ROUTES.LOGIN_LEDGER_NANO_S}><div className='linkBox'>Login using the Ledger Nano S</div></Link>
     <Link to={ROUTES.CREATE_WALLET}><div className='linkBox alt'>Create a new wallet</div></Link>
     <Link to={ROUTES.ENCRYPT_KEY}><div className='linkBox alt'>Encrypt an existing key</div></Link>
     <Link to={ROUTES.SETTINGS}><div className='linkBox alt'>Manage Neon settings</div></Link>
