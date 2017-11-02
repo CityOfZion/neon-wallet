f16ffc0f5ed150734819968a8dd22977d0d1ee99
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-02 23:38:50 +1100

    Use CSS Modules

--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import Notification from '../../components/Notification'
 import classNames from 'classnames'
 import { NOTIFICATION_POSITIONS } from '../../core/constants'
-import './App.scss'
+import styles from './App.scss'
 
 type Props = {
   children: React$Node,
@@ -20,13 +20,13 @@ class App extends Component<Props> {
 
   render () {
     const { children, notification, clearNotification } = this.props
+    const shouldPushTop = notification.isShown && notification.position === NOTIFICATION_POSITIONS.TOP
     return (
       <div>
         <Notification notification={notification} clearNotification={clearNotification} />
-        <div className={classNames(
-          'app-content', {
-            'app-content-push': notification.isShown && notification.position === NOTIFICATION_POSITIONS.TOP
-          })}>{children}</div>
+        <div className={classNames(styles.container, {
+          [styles.pushTop]: shouldPushTop
+        })}>{children}</div>
       </div>
     )
   }
