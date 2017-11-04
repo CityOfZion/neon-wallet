426bff0119aaa6f2475acd62fbb07e9d0e5dff8a
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-05 10:58:53 +1100

    Move home styles to Home.scss

--- app/components/Home/Home.jsx
+++ app/components/Home/Home.jsx
@@ -3,17 +3,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Page from '../Page'
 import { ROUTES } from '../../core/constants'
+import styles from './Home.scss'
+import classNames from 'classnames'
 
 const Home = () =>
-  <Page id='home'>
-    <Link to={ROUTES.LOGIN_LOCAL_STORAGE}><div className='linkBox'>Login using a saved wallet</div></Link>
-    <Link to={ROUTES.LOGIN_NEP2}><div className='linkBox'>Login using an encrypted key</div></Link>
-    <Link to={ROUTES.LOGIN_PRIVATE_KEY}><div className='linkBox'>Login using a private key</div></Link>
-    <Link to={ROUTES.LOGIN_LEDGER_NANO_S}><div className='linkBox'>Login using the Ledger Nano S</div></Link>
-    <Link to={ROUTES.CREATE_WALLET}><div className='linkBox alt'>Create a new wallet</div></Link>
-    <Link to={ROUTES.ENCRYPT_KEY}><div className='linkBox alt'>Encrypt an existing key</div></Link>
-    <Link to={ROUTES.SETTINGS}><div className='linkBox alt'>Manage Neon settings</div></Link>
-    <Link to={ROUTES.LOGIN_TOKEN_SALE}><div className='linkBox alt'>Participate in Token Sale</div></Link>
+  <Page id='home' className={styles.container}>
+    <Link to={ROUTES.LOGIN_LOCAL_STORAGE}>
+      <div className={classNames('linkBox', styles.linkBox)}>Login using a saved wallet</div>
+    </Link>
+    <Link to={ROUTES.LOGIN_NEP2}>
+      <div className={classNames('linkBox', styles.linkBox)}>Login using an encrypted key</div>
+    </Link>
+    <Link to={ROUTES.LOGIN_PRIVATE_KEY}>
+      <div className={classNames('linkBox', styles.linkBox)}>Login using a private key</div>
+    </Link>
+    <Link to={ROUTES.LOGIN_LEDGER_NANO_S}>
+      <div className={classNames('linkBox', styles.linkBox)}>Login using the Ledger Nano S</div>
+    </Link>
+    <Link to={ROUTES.CREATE_WALLET}>
+      <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>Create a new wallet</div>
+    </Link>
+    <Link to={ROUTES.ENCRYPT_KEY}>
+      <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>Encrypt an existing key</div>
+    </Link>
+    <Link to={ROUTES.SETTINGS}>
+      <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>Manage Neon settings</div>
+    </Link>
+    <Link to={ROUTES.LOGIN_TOKEN_SALE}>
+      <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>Participate in Token Sale</div>
+    </Link>
   </Page>
 
 export default Home
