a1e30ea05b0bfc282b45b8cba966252af6d9487f
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-04 22:17:31 +1100

    By request of Ethan - Remove his name from the Footer. We are all equal creators now.

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@
 import React from 'react'
 import styles from './Footer.scss'
 
-const Footer = () => <div className={styles.footer}>Created by Ethan Fast and CoZ. Donations: Adr3XjZ5QDzVJrWvzmsTTchpLRRGSzgS5A</div>
+const Footer = () => <div className={styles.footer}>Created by CoZ. Donations: Adr3XjZ5QDzVJrWvzmsTTchpLRRGSzgS5A</div>
 
 export default Footer
