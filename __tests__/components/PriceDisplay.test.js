bddfec515152d20c32474c9fbdb52bd72d23164f
Silas Albrecht <silas.a.a@gmail.com>
2017-11-17 19:49:33 -0500

    Show NEO/GAS prices review updates

--- __tests__/components/PriceDisplay.test.js
+++ __tests__/components/PriceDisplay.test.js
@@ -1,54 +1,12 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import { mount, shallow } from 'enzyme'
-import PriceDisplay from '../../app/containers/PriceDisplay'
-import { formatFiat } from '../../app/core/formatters'
-
-const initialState = {
-  wallet: {
-    neoPrice: 25.48,
-    gasPrice: 18.10
-  }
-}
-
-const setup = (state = initialState, shallowRender = true) => {
-  const store = configureStore([thunk])(state)
-
-  let wrapper
-  if (shallowRender) {
-    wrapper = shallow(<PriceDisplay store={store} />)
-  } else {
-    wrapper = mount(
-      <Provider store={store}>
-        <PriceDisplay />
-      </Provider>
-    )
-  }
-
-  return {
-    store,
-    wrapper
-  }
-}
+import { mount } from 'enzyme'
+import PriceDisplay from '../../app/components/PriceDisplay'
 
 describe('PriceDisplay', () => {
-  test('renders without crashing', (done) => {
-    const { wrapper } = setup()
+  test('renders and shows prices', (done) => {
+    // Full mount (not shallow) so the snapshot will contain the prices in the rendered html.
+    const wrapper = mount(<PriceDisplay neoPrice={25.48} gasPrice={18.10} />)
     expect(wrapper).toMatchSnapshot()
-    done()
-  })
-  test('correctly renders data from state', (done) => {
-    const { wrapper } = setup(initialState, false)
-
-    const neoPrice = wrapper.find('#neoPrice .price')
-    const gasPrice = wrapper.find('#gasPrice .price')
-    const expectedNeoPrice = formatFiat(initialState.wallet.neoPrice)
-    const expectedGasPrice = formatFiat(initialState.wallet.gasPrice)
-
-    expect(neoPrice.text()).toEqual(`$${expectedNeoPrice}`)
-    expect(gasPrice.text()).toEqual(`$${expectedGasPrice}`)
     done()
   })
 })
--- app/containers/Dashboard/Dashboard.jsx
+++ app/containers/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import SplitPane from 'react-split-pane'
 import FaArrowUpward from 'react-icons/lib/fa/arrow-circle-up'
 import NetworkSwitch from '../NetworkSwitch'
-import PriceDisplay from '../PriceDisplay'
+import PriceDisplay from '../../components/PriceDisplay'
 import WalletInfo from '../WalletInfo'
 import TransactionHistory from '../TransactionHistory'
 import Logout from '../../components/Logout'
@@ -20,6 +20,8 @@ type Props = {
   blockHeight: number,
   net: string,
   address: string,
+  neoPrice: number,
+  gasPrice: number,
   togglePane: Function,
   logout: Function,
 }
@@ -32,7 +34,7 @@ export default class Dashboard extends Component<Props> {
   }
 
   render () {
-    const { sendPane, confirmPane, blockHeight, togglePane, logout } = this.props
+    const { sendPane, confirmPane, blockHeight, togglePane, logout, neoPrice, gasPrice } = this.props
     let sendPaneClosed
     if (sendPane === true) {
       sendPaneClosed = '0%'
@@ -55,7 +57,7 @@ export default class Dashboard extends Component<Props> {
             <div className={styles.title}>
               <img src={logo} width='60px' />
             </div>
-            <PriceDisplay />
+            <PriceDisplay neoPrice={neoPrice} gasPrice={gasPrice} />
             <div className={styles.version}>
               <span className={styles.grey}>Version</span>
               <span className={styles.darker}>{version}</span>
