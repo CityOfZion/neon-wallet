f0bcfc6526bfc44d59b0b0503342fed3718542cf
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-23 23:04:09 +1100

    Move loading indicator to the top

--- app/containers/TransactionHistory/TransactionHistory.jsx
+++ app/containers/TransactionHistory/TransactionHistory.jsx
@@ -22,18 +22,16 @@ export default class TransactionHistory extends Component<Props> {
 
   render () {
     const { transactions, net, explorer, isLoadingTransactions } = this.props
+
     return (
       <div id='transactionInfo' className={styles.transactionInfo}>
-        <div className={classNames(styles.columnHeader, 'columnHeader')}>Transaction History</div>
+        <div className={classNames(styles.columnHeader, 'columnHeader')}>Transaction History {isLoadingTransactions && <Loader className={styles.updateLoader} />}</div>
         <div className={classNames(styles.headerSpacer, 'headerSpacer')} />
-        {isLoadingTransactions
-          ? <Loader />
-          : <Transactions
-            transactions={transactions}
-            net={net}
-            explorer={explorer}
-          />
-        }
+        <Transactions
+          transactions={transactions}
+          net={net}
+          explorer={explorer}
+        />
       </div>
     )
   }
