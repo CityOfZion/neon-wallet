e8389e4d64b616c1ef0c8262554dd011ca9b6731
Silas Albrecht <silas.a.a@gmail.com>
2017-11-23 10:26:48 -0500

    Better formatting for bigger numbers

--- app/core/formatters.js
+++ app/core/formatters.js
@@ -1,16 +1,17 @@
 // @flow
 import { truncateNumber } from './math'
+import numeral from 'numeral'
 
 const GAS_DECIMAL_LENGTH = 7
 const GAS_DECIMAL_SHORT_DISPLAY_LENGTH = 4
 
-const FIAT_DECIMAL_LENGTH = 2
-
 export const formatGAS = (gas: number | string, shortDisplay: boolean = false): string => {
   const decimalLength = shortDisplay ? GAS_DECIMAL_SHORT_DISPLAY_LENGTH : GAS_DECIMAL_LENGTH
-  return truncateNumber(parseFloat(gas), decimalLength).toFixed(decimalLength)
+  const customTruncatedNumber = truncateNumber(parseFloat(gas), decimalLength).toFixed(decimalLength)
+  return numeral(customTruncatedNumber).format('0,0.' + Array(decimalLength + 1).join('0'))
+
 }
 
-export const formatNEO = (neo: number | string): number => parseInt(neo, 10)
+export const formatNEO = (neo: number | string): string => numeral(neo).format('0,0')
 
-export const formatFiat = (value: number | string): string => parseFloat(value).toFixed(FIAT_DECIMAL_LENGTH)
+export const formatFiat = (value: number | string): string => numeral(value).format('0,0.00')
