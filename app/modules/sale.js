d4ff9da50e4be555ab7db4dfcc90a23bbecd185a
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-26 19:37:58 +1100

    Rename rpx to sale, and clean it up

--- app/modules/sale.js
+++ app/modules/sale.js
@@ -1,41 +1,9 @@
 // @flow
 import { getTokenBalance, getAccountFromWIFKey, doMintTokens } from 'neon-js'
 import { showErrorNotification, showInfoNotification, showSuccessNotification } from './notifications'
-import { getAddress, getWif, LOGOUT } from './account'
+import { getWif, LOGOUT } from './account'
 import { getNetwork } from './metadata'
 import { getNeo } from './wallet'
-
-// Constants
-export const UPDATE_RPX_BALANCE = 'UPDATE_RPX_BALANCE'
-
-// Actions
-export function updateRpxBalance (balance: number) {
-  return {
-    type: UPDATE_RPX_BALANCE,
-    payload: { RPX: balance }
-  }
-}
-
-export const refreshTokenBalance = (scriptHash: string, silent: boolean = false) => (dispatch: DispatchType, getState: GetStateType) => {
-  const state = getState()
-  const address = getAddress(state)
-  const net = getNetwork(state)
-
-  // TODO: add other check
-  if (scriptHash.slice(0, 1) !== '0x' && scriptHash.length !== 42) {
-    if (!silent) {
-      dispatch(showErrorNotification({ message: 'Not a valid script hash.' }))
-    }
-    return false
-  }
-  getTokenBalance(net, scriptHash.slice(2, scriptHash.length), address).then((balance) => {
-    dispatch(updateRpxBalance(balance))
-  }).catch((e) => {
-    dispatch(updateRpxBalance(0))
-    dispatch(showErrorNotification({ message: 'There is no ability to display tokens at that script hash.' }))
-    return false
-  })
-}
 
 export const participateInSale = (neoToSend: number, scriptHash: string) => (dispatch: DispatchType, getState: GetStateType) => {
   const state = getState()
@@ -78,21 +46,11 @@ export const participateInSale = (neoToSend: number, scriptHash: string) => (dis
   })
 }
 
-// state getters
-export const getRPX = (state: Object) => state.rpx.RPX
-
 const initialState = {
-  RPX: 0
 }
 
 export default (state: Object = initialState, action: Object) => {
   switch (action.type) {
-    case UPDATE_RPX_BALANCE:
-      const { RPX } = action.payload
-      return {
-        ...state,
-        RPX
-      }
     case LOGOUT:
       return initialState
     default:
