fc92c6266df583077ee071d05a76ec3d46687e32
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-26 22:19:14 +1100

    Load info when modal loads

--- app/components/Modals/TokenInfoModal/TokenInfoModal.jsx
+++ app/components/Modals/TokenInfoModal/TokenInfoModal.jsx
@@ -1,58 +1,60 @@
 // @flow
 import React from 'react'
+
 import BaseModal from '../BaseModal'
+
 import Table from '../../Table'
+import Loader from '../../Loader'
 
 import styles from './TokenInfoModal.scss'
 
 type Props = {
     hideModal: Function,
-    token: TokenType
+    token: TokenWithInfoType,
+    retrieveTokenInfo: Function
 }
 
-const TokenInfoTypeModal = (props: Props) => {
-  const { hideModal, token } = props
-  const { symbol, balance, info: { name, totalSupply, decimals } } = token
-
-  return (
-    <BaseModal
-      title='Token Info'
-      hideModal={hideModal}
-      style={{
-        content: {
-          width: '350px',
-          height: '250px'
-        }
-      }}
-    >
-      <div className={styles.container}>
+const TokenInfoTypeModal = ({ hideModal, token, retrieveTokenInfo }: Props) => (
+  <BaseModal
+    onAfterOpen={() => retrieveTokenInfo(token.symbol)}
+    title='Token Info'
+    hideModal={hideModal}
+    style={{
+      content: {
+        width: '350px',
+        height: '250px'
+      }
+    }}
+  >
+    <div className={styles.container}>
+      {!token.info ? <Loader /> : (
         <Table>
           <tbody>
             <tr>
               <td>Symbol:</td>
-              <td>{symbol}</td>
+              <td>{token.symbol}</td>
             </tr>
             <tr>
               <td>Name:</td>
-              <td>{name}</td>
+              <td>{token.info.name}</td>
             </tr>
             <tr>
               <td>Total Supply:</td>
-              <td>{totalSupply}</td>
+              <td>{token.info.totalSupply}</td>
             </tr>
             <tr>
               <td>Decimals</td>
-              <td>{decimals}</td>
+              <td>{token.info.decimals}</td>
             </tr>
             <tr>
               <td>Balance</td>
-              <td>{balance}</td>
+              <td>{token.balance}</td>
             </tr>
           </tbody>
         </Table>
-      </div>
-    </BaseModal>
-  )
-}
+      )}
+    </div>
+  </BaseModal>
+)
 
 export default TokenInfoTypeModal
