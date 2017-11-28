4497edbe75dc3005d9990f3c31d7654fe33320ee
Evgeny Boxer <evgeny.boxer@gmail.com>
2017-11-28 22:20:21 +1100

    Refactor formatters

--- app/core/formatters.js
+++ app/core/formatters.js
@@ -2,15 +2,25 @@
 import { truncateNumber } from './math'
 import numeral from 'numeral'
 
+const DEFAULT_DECIMAL_LENGTH = 8
 const GAS_DECIMAL_LENGTH = 8
 const GAS_DECIMAL_SHORT_DISPLAY_LENGTH = 4
 
-export const formatGAS = (gas: number | string, shortDisplay: boolean = false): string => {
-  const decimalLength = shortDisplay ? GAS_DECIMAL_SHORT_DISPLAY_LENGTH : GAS_DECIMAL_LENGTH
-  const customTruncatedNumber = truncateNumber(parseFloat(gas), decimalLength).toFixed(decimalLength)
-  return numeral(customTruncatedNumber).format('0,0.' + Array(decimalLength + 1).join('0'))
+export const formatNumber = (value: number | string, precision?: number) => {
+  return precision
+    ? numeral(value).format('0,0.' + Array(precision + 1).join('0'))
+    : numeral(value).format('0,0')
 }
 
-export const formatNEO = (neo: number | string): string => numeral(neo).format('0,0')
+export const truncateToken = (value: number | string, precision?: number = DEFAULT_DECIMAL_LENGTH) => truncateNumber(parseFloat(value), precision).toFixed(precision)
+
+export const truncateGAS = (gas: number | string, precision?: number = GAS_DECIMAL_LENGTH) => truncateNumber(parseFloat(gas), precision).toFixed(precision)
+
+export const formatGAS = (gas: number | string, shortDisplay: boolean = false): string => {
+  const precision = shortDisplay ? GAS_DECIMAL_SHORT_DISPLAY_LENGTH : GAS_DECIMAL_LENGTH
+  return formatNumber(truncateGAS(gas, precision), precision)
+}
+
+export const formatNEO = (neo: number | string): string => formatNumber(neo)
 
 export const formatFiat = (value: number | string): string => numeral(value).format('0,0.00')
