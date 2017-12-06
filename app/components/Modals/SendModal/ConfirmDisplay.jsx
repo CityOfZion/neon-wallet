69afb12341a03823ce319191481344682f41c968
slipo <silas.a.a@gmail.com>
2017-12-06 13:52:17 -0500

    Warn user when address being sent to has no transaction history (#397)
    * Warn user when address being sent to has no transaction history
    
    * Fix: 'State' is defined but never used
    
    * Use async

--- app/components/Modals/SendModal/ConfirmDisplay.jsx
+++ app/components/Modals/SendModal/ConfirmDisplay.jsx
@@ -1,9 +1,12 @@
 // @flow
 import React from 'react'
+import { api } from 'neon-js'
 import Button from '../../Button'
 
+import Loader from '../../Loader'
 import { formatBalance } from '../../../core/formatters'
 import { openExplorerAddress } from '../../../core/explorer'
+import asyncWrap from '../../../core/asyncHelper'
 
 import styles from './SendModal.scss'
 
@@ -17,24 +20,59 @@ type Props = {
   net: NetworkType,
 }
 
-const ConfirmDisplay = ({
-  sendAddress,
-  sendAmount,
-  symbol,
-  confirmTransaction,
-  cancelTransaction,
-  explorer,
-  net
-}: Props) => (
-  <div>
-    <p>Please confirm the following transaction:</p>
-    <p>You are sending <strong>{formatBalance(symbol, sendAmount)} {symbol}</strong> to:</p>
-    <div className={styles.externalLink} onClick={() => openExplorerAddress(net, explorer, sendAddress)}>{sendAddress}</div>
-    <div>
-      <Button onClick={confirmTransaction}>Confirm</Button>
-      <Button cancel onClick={cancelTransaction}>Cancel</Button>
-    </div>
-  </div>
-)
+type State = {
+  addressChecked: boolean,
+  addressCheckedMessage: string
+}
+
+class ConfirmDisplay extends React.Component<Props, State> {
+  state = {
+    addressChecked: false,
+    addressCheckedMessage: ''
+  }
+
+  async checkTransactionHistory (net: NetworkType, address: string) {
+    const [err, transactions] = await asyncWrap(api.neonDB.getTransactionHistory(net, address))
+
+    let message = ''
+    if (err) {
+      message = 'Warning: there was an error verifying the recipient address has activity in its transaction history.'
+    } else if (!transactions || !transactions.length) {
+      message = 'Warning: recipient address has no activity in its transaction history. Please be sure the address is correct before sending.'
+    }
+
+    this.setState({
+      addressChecked: true,
+      addressCheckedMessage: message
+    })
+  }
+
+  componentDidMount () {
+    const { sendAddress, net } = this.props
+    this.checkTransactionHistory(net, sendAddress)
+  }
+
+  render () {
+    const { sendAddress, sendAmount, symbol, confirmTransaction, cancelTransaction, explorer, net } = this.props
+    const { addressChecked, addressCheckedMessage } = this.state
+
+    if (!addressChecked) {
+      return (<Loader />)
+    } else {
+      return (
+        <div>
+          <p>Please confirm the following transaction:</p>
+          <p>You are sending <strong>{formatBalance(symbol, sendAmount)} {symbol}</strong> to:</p>
+          <div className={styles.externalLink} onClick={() => openExplorerAddress(net, explorer, sendAddress)}>{sendAddress}</div>
+          <div>
+            <Button onClick={confirmTransaction}>Confirm</Button>
+            <Button cancel onClick={cancelTransaction}>Cancel</Button>
+          </div>
+          { addressCheckedMessage && <div className={styles.addressWarning}>{addressCheckedMessage}</div> }
+        </div>
+      )
+    }
+  }
+}
 
 export default ConfirmDisplay
