846ef13626715d0d5dd5abbd55a29909254038e1
Matt Huggins <matt.huggins@gmail.com>
2018-01-04 17:29:54 -0500

    Fixed net being undefined sometimes when fetching transaction history (#472)

--- app/containers/TransactionHistory/index.js
+++ app/containers/TransactionHistory/index.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { syncTransactionHistory, getIsLoadingTransactions } from '../../modules/transactions'
+import { getNetwork } from '../../modules/metadata'
 import { getAddress } from '../../modules/account'
 import { getTransactions } from '../../modules/wallet'
 import TransactionHistory from './TransactionHistory'
 
 const mapStateToProps = (state: Object) => ({
+  net: getNetwork(state),
   address: getAddress(state),
   transactions: getTransactions(state),
   isLoadingTransactions: getIsLoadingTransactions(state)
