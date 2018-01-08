d28d70d42e710560dd8963923408dd0e4d0e9dbb
phetter <fet@fet>
2018-01-08 17:24:06 -0500

    rht - redeemable hashpuppy token constant

--- app/core/constants.js
+++ app/core/constants.js
@@ -67,14 +67,16 @@ export const MODAL_TYPES = {
 export const TOKENS_TEST = {
   DBC: 'b951ecbbc5fe37a9c280a76cb0ce0014827294cf',
   RPX: '5b7074e873973a6ed3708862f219a6fbf4d1c411',
-  QLC: '0d821bd7b6d53f5c2b40e217c6defc8bbe896cf5'
+  QLC: '0d821bd7b6d53f5c2b40e217c6defc8bbe896cf5',
+  RHT: '2328008e6f6c7bd157a342e789389eb034d9cbc4'
 }
 
 // MainNet
 export const TOKENS = {
   DBC: 'b951ecbbc5fe37a9c280a76cb0ce0014827294cf',
   RPX: 'ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9',
-  QLC: '0d821bd7b6d53f5c2b40e217c6defc8bbe896cf5'
+  QLC: '0d821bd7b6d53f5c2b40e217c6defc8bbe896cf5',
+  RHT: '2328008e6f6c7bd157a342e789389eb034d9cbc4'
 }
 
 export const DEFAULT_WALLET = {
