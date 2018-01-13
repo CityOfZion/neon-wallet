0eb12e7cec504159d8db6f0a0723807db62300f1
Simone Carletti <weppos@weppos.net>
2018-01-13 14:31:54 -0500

    Add RHT token (Redeemable HashPuppy Token) (#499)

--- app/core/constants.js
+++ app/core/constants.js
@@ -74,6 +74,7 @@ export const TOKENS_TEST = {
 export const TOKENS = {
   DBC: 'b951ecbbc5fe37a9c280a76cb0ce0014827294cf',
   RPX: 'ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9',
+  RHT: '2328008e6f6c7bd157a342e789389eb034d9cbc4',
   QLC: '0d821bd7b6d53f5c2b40e217c6defc8bbe896cf5'
 }
 
