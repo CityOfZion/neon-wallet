0dd31f094532f58b7c52b1992b85b3e4ddad9802
Evgeny Boxer <evgeny.boxer@gmail.com>
2018-01-18 16:27:14 +1100

    fix nep5 transfer

--- app/core/nep5.js
+++ app/core/nep5.js
@@ -3,4 +3,4 @@ import { COIN_DECIMAL_LENGTH } from './formatters'
 import { toBigNumber } from './math'
 
 export const adjustDecimalAmountForTokenTransfer = (value: string): string =>
-  toBigNumber(value).times(10 ** COIN_DECIMAL_LENGTH).toNumber()
+  toBigNumber(value).times(10 ** COIN_DECIMAL_LENGTH).round().toNumber()
