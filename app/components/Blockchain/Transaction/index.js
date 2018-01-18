162700361c5867ecd8b0dd3f3ad13916ac3e9276
Evgeny Boxer <evgeny.boxer@gmail.com>
2018-01-18 15:29:17 +1100

    Fix explorer bugs (#555)

--- app/components/Blockchain/Transaction/index.js
+++ app/components/Blockchain/Transaction/index.js
@@ -2,10 +2,10 @@
 import { connect } from 'react-redux'
 
 import Transaction from './Transaction'
-import { getBlockExplorer, getNetwork } from '../../../modules/metadata'
+import { getBlockExplorer, getNetworkId } from '../../../modules/metadata'
 
 const mapStateToProps = (state: Object) => ({
-  net: getNetwork(state),
+  networkId: getNetworkId(state),
   explorer: getBlockExplorer(state)
 })
 
