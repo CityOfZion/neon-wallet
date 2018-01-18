949d909d8499eccdde44b53f2616eeb666333a4e
David Schwartz <dbschwartz@protonmail.com>
2018-01-18 00:32:02 -0800

    Num fixes 1 (#560)
    * fix send tokens issue
    
    * number format change and add decimals

--- app/core/nep5.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import { toBigNumber } from './math'
-
-export const adjustDecimalAmountForTokenTransfer = (value: string, decimals: number): string =>
-  toBigNumber(value).times(10 ** decimals).round().toNumber()
