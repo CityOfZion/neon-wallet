59d9215fde67e106383e466043fb29f25824a98f
Matt Huggins <matt.huggins@gmail.com>
2018-01-23 04:42:38 -0600

    Replaced price actions/reducers with new actions architecture (#595)

--- app/containers/App/Header/index.js
+++ app/containers/App/Header/index.js
@@ -3,17 +3,21 @@ import { connect, type MapStateToProps } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { compose } from 'recompose'
 
+import withData from '../../../hocs/api/withData'
 import withCurrencyData from '../../../hocs/withCurrencyData'
+import pricesActions from '../../../actions/pricesActions'
 import { logout, getAddress, getLoggedIn } from '../../../modules/account'
-import { getNEOPrice, getGASPrice } from '../../../modules/price'
 
 import Header from './Header'
 
 const mapStateToProps: MapStateToProps<*, *, *> = (state: Object) => ({
   address: getAddress(state),
-  neoPrice: getNEOPrice(state),
-  gasPrice: getGASPrice(state),
   isLoggedIn: getLoggedIn(state)
+})
+
+const mapPricesDataToProps = ({ NEO, GAS }) => ({
+  neoPrice: NEO,
+  gasPrice: GAS
 })
 
 const actionCreators = {
@@ -24,5 +28,6 @@ const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispat
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
+  withData(pricesActions, mapPricesDataToProps),
   withCurrencyData('currencyCode')
 )(Header)
