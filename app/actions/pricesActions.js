ee71e139611efee207ede920e8503fc6d541aa3b
Matt Huggins <matt.huggins@gmail.com>
2018-02-28 22:01:28 -0600

    Updated pricesActions to include all asset/token prices

--- app/actions/pricesActions.js
+++ app/actions/pricesActions.js
@@ -1,16 +1,36 @@
 // @flow
-import { api } from 'neon-js'
+import axios from 'axios'
 
 import createRequestActions from '../util/api/createRequestActions'
-import { ASSETS, DEFAULT_CURRENCY_CODE } from '../core/constants'
+import { DEFAULT_CURRENCY_CODE } from '../core/constants'
 
 type Props = {
   symbols?: Array<string>,
   currency?: string
 }
 
+function mapPrices (tickers, currency) {
+  const mapping = {}
+
+  tickers.forEach((ticker) => {
+    mapping[ticker.symbol] = parseFloat(ticker[`price_${currency.toLowerCase()}`])
+  })
+
+  return mapping
+}
+
+function getPrices (currency) {
+  const url = `https://api.coinmarketcap.com/v1/ticker/?limit=0&convert=${currency.toLowerCase()}`
+
+  return axios.get(url).then((response) => {
+    const { data } = response
+    if (data.error) throw new Error(data.error)
+    return mapPrices(data, currency)
+  })
+}
+
 export const ID = 'PRICES'
 
-export default createRequestActions(ID, ({ symbols = [ASSETS.NEO, ASSETS.GAS], currency = DEFAULT_CURRENCY_CODE }: Props = {}) => (state: Object) => {
-  return api.cmc.getPrices(symbols, currency)
+export default createRequestActions(ID, ({ currency = DEFAULT_CURRENCY_CODE }: Props = {}) => (state: Object) => {
+  return getPrices(currency)
 })
