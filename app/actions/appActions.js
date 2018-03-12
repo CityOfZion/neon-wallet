cb45836327d01c7db2b12801ae1801daefc07ecb
Matt Huggins <matt.huggins@gmail.com>
2018-03-12 16:57:30 -0500

    Fixed balance calculation for non-USD currencies

--- app/actions/appActions.js
+++ app/actions/appActions.js
@@ -2,7 +2,6 @@
 import createBatchActions from '../util/api/createBatchActions'
 import accountsActions from './accountsActions'
 import blockHeightActions from './blockHeightActions'
-import pricesActions from './pricesActions'
 import settingsActions from './settingsActions'
 
 export const ID = 'APP'
@@ -10,6 +9,5 @@ export const ID = 'APP'
 export default createBatchActions(ID, {
   accounts: accountsActions,
   blockHeight: blockHeightActions,
-  prices: pricesActions,
   settings: settingsActions
 })
