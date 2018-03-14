7f6bc5a21308a118267c9aad6de7b4bcdb094f82
Matt Huggins <matt.huggins@gmail.com>
2018-03-13 20:11:23 -0500

    Replaced global neon-js mock with local mocks

--- __tests__/setupTests.js
+++ __tests__/setupTests.js
@@ -2,5 +2,14 @@
 import 'raf/polyfill'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { api } from 'neon-js'
 
 configure({ adapter: new Adapter() })
+
+beforeAll(() => {
+  api.setSwitchFreeze(true)
+})
+
+afterAll(() => {
+  api.setSwitchFreeze(false)
+})
