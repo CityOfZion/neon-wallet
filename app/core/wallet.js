828ecaa99bb32a611dec484654d3bd1c5123ae88
Taconut <alex@alexhulbert.com>
2018-03-17 00:41:03 -0400

    Modified phishing detection method to use axios/async/await instead of node-fetch/promises

--- app/core/wallet.js
+++ app/core/wallet.js
@@ -1,7 +1,7 @@
 // @flow
 import { wallet } from 'neon-js'
 import { map, extend } from 'lodash'
-import fetch from 'node-fetch'
+import axios from 'axios'
 
 import { ASSETS } from './constants'
 import { toBigNumber } from './math'
@@ -9,15 +9,12 @@ import { toBigNumber } from './math'
 const MIN_PASSPHRASE_LEN = 4
 
 let addressBlacklist: Array<string> | null = null
-export const isInBlacklist = (address: string): Promise<boolean> => {
-  if (addressBlacklist !== null) return Promise.resolve(addressBlacklist.includes(address))
-  return fetch('https://raw.githubusercontent.com/CityOfZion/phishing/master/blockedAddresses.json')
-    .catch(() => false)
-    .then(res => res.json())
-    .then(blacklist => {
-      addressBlacklist = blacklist
-      return blacklist.includes(address)
-    })
+export const isInBlacklist = async (address: string): Promise<boolean> => {
+  if (addressBlacklist === null) {
+    const { data } = await axios.get('https://raw.githubusercontent.com/CityOfZion/phishing/master/blockedAddresses.json')
+    addressBlacklist = data
+  }
+  return addressBlacklist.includes(address)
 }
 
 export const validatePassphraseLength = (passphrase: string): boolean =>
