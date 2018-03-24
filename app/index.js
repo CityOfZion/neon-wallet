93612cf08049b734446f3a16554cf1e64ef73110
Matt Huggins <matt.huggins@gmail.com>
2018-03-24 12:03:42 -0500

    Updated react-hot-loader to fix async autobind functions

--- app/index.js
+++ app/index.js
@@ -1,27 +1,10 @@
 import 'raf/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+
 import store from './store/configureStore'
 import Root from './Root'
 import './styles/tippy.compiled.global.css'
 import './styles/main.global.scss'
 
-render(
-  <AppContainer>
-    <Root store={store} />
-  </AppContainer>,
-  document.getElementById('root')
-)
-
-if (module.hot) {
-  module.hot.accept('./Root', () => {
-    const NewRoot = require('./Root').default
-    render(
-      <AppContainer>
-        <NewRoot store={store} />
-      </AppContainer>,
-      document.getElementById('root')
-    )
-  })
-}
+render(<Root store={store} />, document.getElementById('root'))
