3a2d2638da2d83d99f6e6f84bb5478d87ee3e6e5
Matt Huggins <matt.huggins@gmail.com>
2018-03-24 12:03:50 -0500

    Updated blacklist check to use async/await

--- app/Root/Root.jsx
+++ app/Root/Root.jsx
@@ -1,8 +1,9 @@
 // @flow
 import React from 'react'
 import { Provider } from 'react-redux'
-import Routes from './Routes'
 import { BrowserRouter } from 'react-router-dom'
+
+import Routes from '../Routes'
 
 type Props = {
   store: Object,
