d21e95d9d9ed2e87fb36b4971456ce039da2d218
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 15:54:48 -0500

    Reordered routes logically

--- app/components/Root/Routes.jsx
+++ app/components/Root/Routes.jsx
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 
 import PrivateRoute from './PrivateRoute'
 import App from '../../containers/App'
+import Home from '../../containers/Home'
 import LoginPrivateKey from '../../containers/LoginPrivateKey'
 import LoginLedgerNanoS from '../../containers/LoginLedgerNanoS'
 import CreateWallet from '../../containers/CreateWallet'
@@ -14,7 +15,6 @@ import LoginNep2 from '../../containers/LoginNep2'
 import Settings from '../../containers/Settings'
 import DisplayWalletAccounts from '../../containers/DisplayWalletAccounts'
 import TransactionHistory from '../../containers/TransactionHistory'
-import Home from '../../containers/Home'
 import { ROUTES } from '../../core/constants'
 
 const CreateWalletEncrypted = (props) => <CreateWallet {...props} encryptWIF />
@@ -22,18 +22,18 @@ const CreateWalletEncrypted = (props) => <CreateWallet {...props} encryptWIF />
 export default () => (
   <App>
     <Switch>
-      <PrivateRoute exact path={ROUTES.DASHBOARD} component={Dashboard} />
-      <PrivateRoute exact path={ROUTES.RECEIVE} component={Receive} />
+      <Route exact path={ROUTES.HOME} component={Home} />
       <Route exact path={ROUTES.CREATE_WALLET} component={CreateWallet} />
       <Route exact path={ROUTES.ENCRYPT_KEY} component={CreateWalletEncrypted} />
       <Route exact path={ROUTES.LOGIN_PRIVATE_KEY} component={LoginPrivateKey} />
       <Route exact path={ROUTES.LOGIN_LOCAL_STORAGE} component={LoginLocalStorage} />
       <Route exact path={ROUTES.LOGIN_NEP2} component={LoginNep2} />
       <Route exact path={ROUTES.LOGIN_LEDGER_NANO_S} component={LoginLedgerNanoS} />
-      <Route exact path={ROUTES.SETTINGS} component={Settings} />
       <Route exact path={ROUTES.DISPLAY_WALLET_KEYS} component={DisplayWalletAccounts} />
+      <Route exact path={ROUTES.SETTINGS} component={Settings} />
+      <PrivateRoute exact path={ROUTES.DASHBOARD} component={Dashboard} />
+      <PrivateRoute exact path={ROUTES.RECEIVE} component={Receive} />
       <PrivateRoute exact path={ROUTES.TRANSACTION_HISTORY} component={TransactionHistory} />
-      <Route exact path={ROUTES.HOME} component={Home} />
       <Redirect to={ROUTES.DASHBOARD} />
     </Switch>
   </App>
