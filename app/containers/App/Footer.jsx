e97ebbf570ad5b734ddc9ed7ff5f020f41db6225
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 14:44:44 -0500

    Fixed community support link in footer

--- app/containers/App/Footer.jsx
+++ app/containers/App/Footer.jsx
@@ -7,23 +7,26 @@ import NetworkSwitch from './Sidebar/NetworkSwitch'
 
 import styles from './Footer.scss'
 
-const openDiscordLink = () => shell.openExternal('https://discordapp.com/invite/R8v48YA')
-
 type Props = {
   className?: string
 }
 
-const Footer = ({ className }: Props) => {
-  return (
-    <div className={classNames(styles.footer, className)}>
-      <span className={styles.item}>
-        <NetworkSwitch />
-      </span>
-      <span className={styles.item}>
-        <a href='#' onClick={openDiscordLink}>Community Support</a>
-      </span>
-    </div>
-  )
-}
+export default class Footer extends React.Component<Props> {
+  render () {
+    return (
+      <div className={classNames(styles.footer, this.props.className)}>
+        <span className={styles.item}>
+          <NetworkSwitch />
+        </span>
+        <span className={styles.item}>
+          <a href='#' onClick={this.handleSupport}>Community Support</a>
+        </span>
+      </div>
+    )
+  }
 
-export default Footer
+  handleSupport = (event: Object) => {
+    event.preventDefault()
+    shell.openExternal('https://discordapp.com/invite/R8v48YA')
+  }
+}
