5065eaa1097bc0b2dee0e3053d22167074c3810e
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 15:53:51 -0500

    Added PrivateRoute to prevent viewing internal pages while unauthenticated

---
+++ app/components/Root/PrivateRoute/index.js
@@ -0,0 +1,16 @@
+// @flow
+import { compose, mapProps } from 'recompose'
+import { withRouter } from 'react-router-dom'
+import { withProgress, progressValues } from 'spunky'
+import { omit } from 'lodash'
+
+import PrivateRoute from './PrivateRoute'
+import authActions from '../../../actions/authActions'
+
+const { LOADED } = progressValues
+
+export default compose(
+  withRouter,
+  withProgress(authActions, { propName: 'progress' }),
+  mapProps((props) => ({ ...omit(props, 'progress'), authenticated: props.progress === LOADED }))
+)(PrivateRoute)
