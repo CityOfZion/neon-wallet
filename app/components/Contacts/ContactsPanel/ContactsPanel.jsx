c561c1f913e2d2ed7be4e649447f0c931577d537
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 14:44:20 -0500

    Added contacts panel to contacts page

---
+++ app/components/Contacts/ContactsPanel/ContactsPanel.jsx
@@ -0,0 +1,79 @@
+// @flow
+import React from 'react'
+import { map } from 'lodash'
+
+import Panel from '../../Panel'
+import Button from '../../Button'
+import AddIcon from '../../../assets/icons/contacts-add.svg'
+import EditIcon from '../../../assets/icons/edit.svg'
+import DeleteIcon from '../../../assets/icons/delete.svg'
+import styles from './ContactsPanel.scss'
+
+type Props = {
+  contacts: {
+    [name: string]: string
+  }
+}
+
+export default class ContactsPanel extends React.Component<Props> {
+  render () {
+    return (
+      <Panel className={styles.contactsPanel} renderHeader={this.renderHeader}>
+        <div className={styles.contacts}>
+          {map(this.props.contacts, this.renderContact)}
+        </div>
+      </Panel>
+    )
+  }
+
+  renderHeader = () => {
+    return (
+      <div className={styles.header}>
+        <span>Contacts</span>
+        <span id='add' className={styles.addButton} onClick={this.handleAdd}>
+          <AddIcon className={styles.addIcon} />
+          <span>New Contact</span>
+        </span>
+      </div>
+    )
+  }
+
+  renderContact = (address: string, name: string) => {
+    return (
+      <div key={address} className={styles.contact}>
+        <div className={styles.details}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.address}>{address}</div>
+        </div>
+        <div className={styles.actions}>
+          <Button
+            className={styles.editButton}
+            renderIcon={EditIcon}
+            onClick={this.handleEdit}
+          >
+            Edit
+          </Button>
+          <Button
+            className={styles.deleteButton}
+            renderIcon={DeleteIcon}
+            onClick={this.handleDelete}
+          >
+            Delete Contact
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  handleAdd = () => {
+    // TODO
+  }
+
+  handleEdit = () => {
+    // TODO
+  }
+
+  handleDelete = () => {
+    // TODO
+  }
+}
