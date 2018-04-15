8cb8d39b7682dd7e2625f91df8a34de9afd6016d
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 14:44:03 -0500

    Added contacts route

---
+++ app/containers/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Contacts.scss'
+
+export default class Contacts extends React.Component {
+  render () {
+    return (
+      <div className={styles.contacts}>
+        Contacts
+      </div>
+    )
+  }
+}
