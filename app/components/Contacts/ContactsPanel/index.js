a2d7c5c0d67264389ea345c12d6794c352fc05ca
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 17:58:30 -0500

    Added delete contact functionality

--- app/components/Contacts/ContactsPanel/index.js
+++ app/components/Contacts/ContactsPanel/index.js
@@ -1,1 +1,11 @@
-export { default } from './ContactsPanel'
+// @flow
+import { connect } from 'react-redux'
+
+import ContactsPanel from './ContactsPanel'
+import { deleteAddress } from '../../../modules/addressBook'
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteContact: (...args) => dispatch(deleteAddress(...args))
+})
+
+export default connect(null, mapDispatchToProps)(ContactsPanel)
