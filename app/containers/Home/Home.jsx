b5c23eb9dab17ee2f5c7ff1dcee82369d480ed35
Maxwell Lasky <mlasky46@gmail.com>
2018-04-28 14:07:20 -0600

    fixed console.log and deleted dead code in Home.jsx

--- app/containers/Home/Home.jsx
+++ app/containers/Home/Home.jsx
@@ -34,7 +34,7 @@ class Home extends Component<Props, State> {
       case 'Private Key':
         return loginWithPrivateKey(this.state.password)
       default:
-        console.log('foo')
+        return console.warn('handleSubmit() invoked with an invalid option!')
     }
   }
 
@@ -90,54 +90,3 @@ class Home extends Component<Props, State> {
 }
 
 export default Home
-
-// // @flow
-// import React from 'react'
-// import classNames from 'classnames'
-// import { Link } from 'react-router-dom'
-//
-// import { ROUTES } from '../../core/constants'
-//
-// import styles from './Home.scss'
-//
-// const Home = () => (
-//   <div id='home'>
-//     <Link to={ROUTES.LOGIN_LOCAL_STORAGE}>
-//       <div className={classNames('linkBox', styles.linkBox)}>
-//         Login using a saved wallet
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.LOGIN_NEP2}>
-//       <div className={classNames('linkBox', styles.linkBox)}>
-//         Login using an encrypted key
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.LOGIN_PRIVATE_KEY}>
-//       <div className={classNames('linkBox', styles.linkBox)}>
-//         Login using a private key
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.LOGIN_LEDGER_NANO_S}>
-//       <div className={classNames('linkBox', styles.linkBox)}>
-//         Login using a Ledger
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.CREATE_WALLET}>
-//       <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>
-//         Create a new wallet
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.ENCRYPT_KEY}>
-//       <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>
-//         Encrypt an existing key
-//       </div>
-//     </Link>
-//     <Link to={ROUTES.SETTINGS}>
-//       <div className={classNames('linkBox', styles.linkBox, styles.linkBoxAlt)}>
-//         Manage Neon settings
-//       </div>
-//     </Link>
-//   </div>
-// )
-//
-// export default Home
