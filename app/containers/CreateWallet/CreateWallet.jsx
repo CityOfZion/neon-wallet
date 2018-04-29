bc4fbe573be90dc68f2d02247986071ba6904da0
Maxwell Lasky <mlasky46@gmail.com>
2018-04-29 16:09:45 -0600

    routing set up

--- app/containers/CreateWallet/CreateWallet.jsx
+++ app/containers/CreateWallet/CreateWallet.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import PasswordInput from '../../components/Inputs/PasswordInput'
 import HomeButtonLink from '../../components/HomeButtonLink'
 import Button from '../../components/Button'
+import styles from '../Home/Home.scss'
+import neonLogo from '../../images/neon-logo-redesign.png'
+import AddIcon from '../../assets/icons/add.svg'
 
 type Props = {
   encryptWIF: boolean,
@@ -14,7 +17,7 @@ type Props = {
 type State = {
   passphrase: string,
   passphrase2: string,
-  wif: string,
+  wif: string
 }
 
 export default class EncryptKey extends React.Component<Props, State> {
@@ -29,12 +32,19 @@ export default class EncryptKey extends React.Component<Props, State> {
     const { encryptWIF, history } = this.props
     const { passphrase, passphrase2, wif } = this.state
     const { generateNewWalletAccount } = this.props
-    if (!generateNewWalletAccount(passphrase, passphrase2, encryptWIF ? wif : null, history)) {
+    if (
+      !generateNewWalletAccount(
+        passphrase,
+        passphrase2,
+        encryptWIF ? wif : null,
+        history
+      )
+    ) {
       // this.resetFields()
     }
   }
 
-  resetFields () {
+  resetFields() {
     this.setState({
       passphrase: '',
       passphrase2: '',
@@ -42,7 +52,7 @@ export default class EncryptKey extends React.Component<Props, State> {
     })
   }
 
-  render () {
+  render() {
     const { encryptWIF } = this.props
     const { passphrase, passphrase2, wif } = this.state
     let disabledButton
@@ -57,30 +67,51 @@ export default class EncryptKey extends React.Component<Props, State> {
     }
 
     return (
-      <div id="newWallet">
-        <div className="info">{title}</div>
-        <form onSubmit={this.createWalletAccount}>
-          <PasswordInput
-            value={passphrase}
-            onChange={(e) => this.setState({ passphrase: e.target.value })}
-            placeholder="Enter passphrase here"
-            autoFocus
-          />
-          <PasswordInput
-            value={passphrase2}
-            onChange={(e) => this.setState({ passphrase2: e.target.value })}
-            placeholder="Enter passphrase again"
-          />
-          {encryptWIF &&
-            <PasswordInput
-              value={wif}
-              onChange={(e) => this.setState({ wif: e.target.value })}
-              placeholder="Enter existing WIF here"
-            />
-          }
-          <Button type="submit" primary disabled={disabledButton}>Generate keys</Button>
-          <HomeButtonLink />
-        </form>
+      <div id="home" className={styles.homeContainer}>
+        <div className={styles.innerHomeContainer}>
+          <img className={styles.logo} src={neonLogo} />
+          <div className={styles.inputContainer}>
+            <div id="createWallet" className={styles.flexContainer}>
+              <form onSubmit={this.createWalletAccount}>
+                <div className={styles.inputMargin}>
+                  <PasswordInput
+                    value={passphrase}
+                    onChange={e =>
+                      this.setState({ passphrase: e.target.value })
+                    }
+                    placeholder="Enter passphrase here"
+                    autoFocus
+                  />
+                </div>
+                <div className={styles.inputMargin}>
+                  <PasswordInput
+                    value={passphrase2}
+                    onChange={e =>
+                      this.setState({ passphrase2: e.target.value })
+                    }
+                    placeholder="Enter passphrase again"
+                  />
+                  {encryptWIF && (
+                    <PasswordInput
+                      value={wif}
+                      onChange={e => this.setState({ wif: e.target.value })}
+                      placeholder="Enter existing WIF here"
+                    />
+                  )}
+                </div>
+                <Button
+                  renderIcon={AddIcon}
+                  type="submit"
+                  primary
+                  disabled={disabledButton}
+                >
+                  Create Wallet
+                </Button>
+                <HomeButtonLink />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
