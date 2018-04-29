d04c180dcaf565c95ad086d1970be6b1d138b387
Maxwell Lasky <mlasky46@gmail.com>
2018-04-28 18:51:26 -0600

    fixed orientation of ledger auth

--- app/containers/LoginLedgerNanoS/LoginLedgerNanoS.jsx
+++ app/containers/LoginLedgerNanoS/LoginLedgerNanoS.jsx
@@ -27,25 +27,26 @@ const POLL_FREQUENCY = 1000
 export default class LoginLedgerNanoS extends React.Component<Props> {
   intervalId: ?number
 
-  componentDidMount () {
+  componentDidMount() {
     this.intervalId = setInterval(this.props.connect, POLL_FREQUENCY)
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     if (this.intervalId) {
       clearInterval(this.intervalId)
     }
   }
 
-  render () {
+  render() {
     return (
       <div id="loginLedgerNanoS" className={styles.flexContainer}>
         <div className={styles.loginForm}>
+          {this.renderStatus()}
           <div>
             <Button
               renderIcon={() => <Login />}
               icon="login"
-              style={{ marginTop: 10 }}
+              style={{ marginTop: 20 }}
               id="loginButton"
               primary
               type="submit"
@@ -55,18 +56,17 @@ export default class LoginLedgerNanoS extends React.Component<Props> {
               Login
             </Button>
           </div>
-          {this.renderStatus()}
         </div>
       </div>
     )
   }
 
-  renderStatus () {
+  renderStatus() {
     const { progress, deviceInfo, error } = this.props
 
     if (progress === LOADED && deviceInfo) {
       return (
-        <p style={{ fontWeight: 'lighter' }}>
+        <p style={{ fontWeight: 'lighter', marginTop: 0 }}>
           Found USB {deviceInfo.manufacturer} {deviceInfo.product}. NEO app
           found on hardward device. Click button above to login.
         </p>
@@ -74,11 +74,15 @@ export default class LoginLedgerNanoS extends React.Component<Props> {
     }
 
     if (progress === FAILED && error) {
-      return <p style={{ fontWeight: 'lighter', color: 'red' }}>{error}</p>
+      return (
+        <p style={{ fontWeight: 'lighter', color: 'red', marginTop: 0 }}>
+          {error}
+        </p>
+      )
     }
 
     return (
-      <p style={{ fontWeight: 'lighter' }}>
+      <p style={{ fontWeight: 'lighter', marginTop: 0 }}>
         Looking for USB Devices. Please plugin your device and login.
       </p>
     )
@@ -88,7 +92,7 @@ export default class LoginLedgerNanoS extends React.Component<Props> {
     this.props.login(this.props.publicKey)
   }
 
-  canLogin () {
+  canLogin() {
     return this.props.progress === LOADED
   }
 }
