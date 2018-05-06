9dc0d5f0d5ea6c14e6583b7dbe3f1561ce4a747c
Maxwell Lasky <mlasky46@gmail.com>
2018-05-06 15:10:25 -0600

    remove DisplayWalletAccounts and all references to it

--- app/components/Root/Routes.jsx
+++ app/components/Root/Routes.jsx
@@ -12,19 +12,14 @@ import Contacts from '../../containers/Contacts'
 import AddContact from '../../containers/AddContact'
 import EditContact from '../../containers/EditContact'
 import Settings from '../../containers/Settings'
-import DisplayWalletAccounts from '../../containers/DisplayWalletAccounts'
 import TransactionHistory from '../../containers/TransactionHistory'
 import { ROUTES } from '../../core/constants'
-
-const CreateWalletEncrypted = (props) => <CreateWallet {...props} encryptWIF />
 
 export default () => (
   <App>
     <Switch>
       <Route exact path={ROUTES.HOME} component={Home} />
       <Route exact path={ROUTES.CREATE_WALLET} component={CreateWallet} />
-      <Route exact path={ROUTES.ENCRYPT_KEY} component={CreateWalletEncrypted} />
-      <Route exact path={ROUTES.DISPLAY_WALLET_KEYS} component={DisplayWalletAccounts} />
       <Route exact path={ROUTES.SETTINGS} component={Settings} />
       <PrivateRoute exact path={ROUTES.DASHBOARD} component={Dashboard} />
       <PrivateRoute exact path={ROUTES.RECEIVE} component={Receive} />
