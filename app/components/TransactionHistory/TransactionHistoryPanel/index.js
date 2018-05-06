4ba7ee6223e5035a079addc2e28ed30d82657fb0
Matt Huggins <matt.huggins@gmail.com>
2018-05-06 18:48:34 -0500

    Moved transaction history request to container

--- app/components/TransactionHistory/TransactionHistoryPanel/index.js
+++ app/components/TransactionHistory/TransactionHistoryPanel/index.js
@@ -1,11 +1,9 @@
 // @flow
 import { compose } from 'recompose'
-import { withCall, withData } from 'spunky'
+import { withData } from 'spunky'
 
 import TransactionHistoryPanel from './TransactionHistoryPanel'
 import transactionHistoryActions from '../../../actions/transactionHistoryActions'
-import withNetworkData from '../../../hocs/withNetworkData'
-import withAuthData from '../../../hocs/withAuthData'
 import withProgressPanel from '../../../hocs/withProgressPanel'
 
 const mapTransactionsDataToProps = (transactions) => ({
@@ -13,9 +11,6 @@ const mapTransactionsDataToProps = (transactions) => ({
 })
 
 export default compose(
-  withNetworkData(),
-  withAuthData(),
-  withCall(transactionHistoryActions),
-  withData(transactionHistoryActions, mapTransactionsDataToProps),
-  withProgressPanel(transactionHistoryActions, { title: 'Transaction History' })
+  withProgressPanel(transactionHistoryActions, { title: 'Transaction History' }),
+  withData(transactionHistoryActions, mapTransactionsDataToProps)
 )(TransactionHistoryPanel)
