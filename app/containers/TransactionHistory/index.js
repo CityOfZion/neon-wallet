281db34cbeedb5e0fe35074b418a7edab6801941
Matt Huggins <matt.huggins@gmail.com>
2018-05-16 20:07:30 -0500

    Updated transaction history panel to gracefully handle failure

--- app/containers/TransactionHistory/index.js
+++ app/containers/TransactionHistory/index.js
@@ -1,6 +1,7 @@
 // @flow
+import React from 'react'
 import { compose, withProps } from 'recompose'
-import { withData, withProgress, progressValues } from 'spunky'
+import { withData, withProgress, withProgressComponents, alreadyLoadedStrategy, progressValues } from 'spunky'
 
 import TransactionHistory from './TransactionHistory'
 import transactionHistoryActions from '../../actions/transactionHistoryActions'
@@ -8,7 +9,7 @@ import withNetworkData from '../../hocs/withNetworkData'
 import withAuthData from '../../hocs/withAuthData'
 import withoutProps from '../../hocs/withoutProps'
 
-const { LOADING } = progressValues
+const { LOADING, FAILED } = progressValues
 
 const PROGRESS_PROP = 'progress'
 
@@ -20,13 +21,20 @@ const mapLoadingProp = (props) => ({
   loading: props[PROGRESS_PROP] === LOADING
 })
 
+const Failed = <div>Failed to load.</div>
+
 export default compose(
   withNetworkData(),
   withAuthData(),
-  withData(transactionHistoryActions, mapTransactionsDataToProps),
 
   // pass `loading` boolean to component
   withProgress(transactionHistoryActions, { propName: PROGRESS_PROP }),
+  withProgressComponents(transactionHistoryActions, {
+    [FAILED]: Failed
+  }, {
+    strategy: alreadyLoadedStrategy
+  }),
   withProps(mapLoadingProp),
+  withData(transactionHistoryActions, mapTransactionsDataToProps),
   withoutProps(PROGRESS_PROP)
 )(TransactionHistory)
