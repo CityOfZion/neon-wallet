88305892c104af8521b56b47d1b83ff696bd0319
miguelzf <eligmu@gmail.com>
2018-06-02 01:39:07 +0100

    Deprecate Warning patch: prune list of deprecated tokens. Contract creator can add their own if they want.

--- app/core/constants.js
+++ app/core/constants.js
@@ -91,13 +91,13 @@ export const ENDED_ICO_TOKENS = [
 ]
 
 export const DEPRECATED_TOKENS = [
-  '4b4f63919b9ecfd2483f0c72ff46ed31b5bbb7a4', // Phantasma SOUL
-  'a0b328c01eac8b12b0f8a4fe93645d18fb3f1f0a', // NKN
-  '7ac4a2bb052a047506f2f2d3d1528b89cc38e8d4', // quarteria
-  '23501e5fef0f67ec476406c556e91992323a0357', // Orbis
-  '442e7964f6486005235e87e082f56cd52aa663b8', // Ontology
-  '2e25d2127e0240c6deaf35394702feb236d4d7fc', // Narrative
-  '6d36b38af912ca107f55a5daedc650054f7e4f75' // Aphelion
+  '4b4f63919b9ecfd2483f0c72ff46ed31b5bbb7a4' // Phantasma SOUL
+  // 'a0b328c01eac8b12b0f8a4fe93645d18fb3f1f0a', // NKN
+  // '7ac4a2bb052a047506f2f2d3d1528b89cc38e8d4', // quarteria
+  // '23501e5fef0f67ec476406c556e91992323a0357', // Orbis
+  // '442e7964f6486005235e87e082f56cd52aa663b8', // Ontology
+  // '2e25d2127e0240c6deaf35394702feb236d4d7fc' // Narrative
+  // '6d36b38af912ca107f55a5daedc650054f7e4f75' // Aphelion
 ]
 
 export const DEFAULT_WALLET = {
