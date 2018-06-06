20da4ff24d4d7ff196179867fafa31d7863820f8
Evgeny Boxer <evgeny.boxer@gmail.com>
2018-06-06 12:39:29 +1000

    Verify total is a type BigNumber

--- app/actions/claimsActions.js
+++ app/actions/claimsActions.js
@@ -1,5 +1,5 @@
 // @flow
-import { isNil } from 'lodash'
+import BigNumber from 'bignumber.js'
 import { api } from 'neon-js'
 import { createActions } from 'spunky'
 
@@ -11,6 +11,6 @@ type Props = {
 export const ID = 'CLAIMS'
 
 export default createActions(ID, ({ net, address }: Props = {}) => async (state: Object): Promise<Object> => {
-  const total = await api.getMaxClaimAmountFrom({ net, address }, api.neoscan)
-  return { total: !isNil(total) ? total.toString() : null }
+  const total: BigNumber = await api.getMaxClaimAmountFrom({ net, address }, api.neoscan)
+  return { total: total instanceof BigNumber ? total.toString() : null }
 })
