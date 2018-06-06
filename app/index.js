e2d663720481132511f1210700d3188f8ad4de65
Alex Hulbert <alex@alexhulbert.com>
2018-06-06 02:46:34 -0400

    Changed font to Montserrat (fixes #1007) (#1194)

--- app/index.js
+++ app/index.js
@@ -6,5 +6,6 @@ import store from './store/configureStore'
 import Root from './components/Root'
 import './styles/tippy.compiled.global.css'
 import './styles/main.global.scss'
+import 'typeface-montserrat'
 
 render(<Root store={store} />, document.getElementById('root'))
