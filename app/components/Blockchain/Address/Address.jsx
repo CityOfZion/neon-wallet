116f2b6a5585e873e4bca88fe615600544a9560a
Austin Hinderer <zeshon@gmail.com>
2018-06-05 19:41:38 -0700

    Updates for lint and flow

--- app/components/Blockchain/Address/Address.jsx
+++ app/components/Blockchain/Address/Address.jsx
@@ -13,23 +13,25 @@ type Props = {
 }
 
 export default class Address extends React.Component<Props> {
-  constructor(props) {
+  handleClick: Function
+
+  constructor (props: Props) {
     super(props)
 
     this.handleClick = this.handleClick.bind(this)
   }
 
-  render() {
+  render () {
     const { address, className } = this.props
 
     return (
-      <span className={ classNames(styles.address, className) } onClick={ this.handleClick }>
+      <span className={classNames(styles.address, className)} onClick={this.handleClick}>
         { address }
       </span>
     )
   }
 
-  handleClick() {
+  handleClick () {
     const { networkId, explorer, address } = this.props
     openExplorerAddress(networkId, explorer, address)
   }
