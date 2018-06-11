eabd642a31aa0831b258462799382ea93b2f8642
Evgeny Boxer <evgeny.boxer@gmail.com>
2018-06-11 11:01:08 +1000

    fix(claims): Fixes max claim amount test

--- app/actions/claimsActions.js
+++ app/actions/claimsActions.js
@@ -1,6 +1,5 @@
 // @flow
-import BigNumber from 'bignumber.js'
-import { api } from 'neon-js'
+import { api, u } from 'neon-js'
 import { createActions } from 'spunky'
 
 type Props = {
@@ -11,6 +10,6 @@ type Props = {
 export const ID = 'CLAIMS'
 
 export default createActions(ID, ({ net, address }: Props = {}) => async (state: Object): Promise<Object> => {
-  const total: BigNumber = await api.getMaxClaimAmountFrom({ net, address }, api.neoscan)
-  return { total: total instanceof BigNumber ? total.toString() : null }
+  const total = await api.getMaxClaimAmountFrom({ net, address }, api.neoscan)
+  return { total: total instanceof u.Fixed8 ? total.toString() : null }
 })
