d28c9a06200cb1afb9d80880f9d0dd0b774e8108
Maxwell Lasky <mlasky46@gmail.com>
2018-06-27 09:15:39 -0600

    adds back blockHeightActions.js

---
+++ app/actions/blockHeightActions.js
@@ -0,0 +1,21 @@
+// @flow
+import { api, rpc } from 'neon-js'
+import { createActions } from 'spunky'
+
+import { getNetworkById } from '../core/deprecated'
+
+type Props = {
+  networkId: string
+}
+
+export const ID = 'BLOCK_HEIGHT'
+
+export default createActions(
+  ID,
+  ({ networkId }: Props = {}) => async (state: Object) => {
+    const net = getNetworkById(networkId)
+    const endpoint = await api.getRPCEndpointFrom({ net }, api.neoscan)
+    const client = new rpc.RPCClient(endpoint)
+    return client.getBlockCount()
+  }
+)
