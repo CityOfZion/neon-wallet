15e9f9627bb0e210e8dd69bee3f091661151cce7
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-03 23:01:16 +0200

    Begin markup for SendAmountsPanel

---
+++ app/components/Send/SendAmountsPanel/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import styles from './SendAmountsPanel.scss'
+
+class SendPageHeader extends Component {
+  render() {
+    return <section className={styles.sendAmountsPanel}>Hello</section>
+  }
+}
+export default SendPageHeader
