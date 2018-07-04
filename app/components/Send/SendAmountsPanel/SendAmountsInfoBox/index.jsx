9275e5f92b852f846b4b64783db3caa7cdb91b51
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-04 13:25:08 +0200

    Preliminary markup and styling for SendAmountsPanel

---
+++ app/components/Send/SendAmountsPanel/SendAmountsInfoBox/index.jsx
@@ -0,0 +1,42 @@
+// @flow
+import React from 'react'
+
+import styles from './SendAmountsInfoBox.scss'
+
+type Props = {
+  assetName: string,
+  assetPrice: number,
+  totalAmount: number,
+  remainingAmount: number,
+  fiatCurrencySymbol: string
+}
+
+const SendAmountsInfoBox = ({
+  assetName,
+  assetPrice,
+  totalAmount,
+  remainingAmount,
+  fiatCurrencySymbol
+}: Props) => (
+  <div className={styles.sendAmountsInfoBox}>
+    <div className={styles.assetTitle}>
+      <h3>{assetName}</h3>
+    </div>
+    <div className={styles.assetAmounts}>
+      <p className={styles.assetAmountsPrimary}>{totalAmount}</p>
+      <p className={styles.assetAmountsSecondary}>{remainingAmount}</p>
+    </div>
+    <div className={styles.assetValue}>
+      <p className={styles.totalAssetValue}>
+        {fiatCurrencySymbol}
+        {assetPrice * totalAmount}
+      </p>
+      <p className={styles.remainingAssetValue}>
+        {fiatCurrencySymbol}
+        {assetPrice * remainingAmount}
+      </p>
+    </div>
+  </div>
+)
+
+export default SendAmountsInfoBox
--- app/components/Send/SendAmountsPanel/index.jsx
+++ app/components/Send/SendAmountsPanel/index.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
+import SendAmountsInfoBox from './SendAmountsInfoBox'
+
 import styles from './SendAmountsPanel.scss'
 
 class SendPageHeader extends Component {
   render() {
-    return <section className={styles.sendAmountsPanel}>Hello</section>
+    return (
+      <section className={styles.sendAmountsPanel}>
+        <SendAmountsInfoBox
+          assetName="NEO"
+          assetPrice={42}
+          totalAmount={100}
+          remainingAmount={100}
+          fiatCurrencySymbol="$"
+        />
+        <SendAmountsInfoBox
+          assetName="RPX"
+          assetPrice={5}
+          totalAmount={5123}
+          remainingAmount={5123}
+          fiatCurrencySymbol="$"
+        />
+      </section>
+    )
   }
 }
 export default SendPageHeader
