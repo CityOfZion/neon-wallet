6cb61ae7817ecb1e293fa98dc22b48a665f4804c
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-05 15:13:07 +0200

    Create data flow from Send to SendRecipientList

--- app/components/Send/SendPanel/SendRecipientList/index.jsx
+++ app/components/Send/SendPanel/SendRecipientList/index.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react'
 import classNames from 'classnames'
 
@@ -5,31 +7,57 @@ import SendRecipientListItem from './SendRecipientListItem'
 
 import styles from './SendRecipientList.scss'
 
-const SendRecipientList = () => (
-  <section className={styles.sendRecipientListContainer}>
-    <div className={styles.sendRecipientListHeaders}>
-      <h3 className={classNames(styles.sendRecipientListHeader, styles.asset)}>
-        Asset
-      </h3>
-      <h3 className={classNames(styles.sendRecipientListHeader, styles.amount)}>
-        Amount
-      </h3>
-      <h3
-        className={classNames(styles.sendRecipientListHeader, styles.address)}
-      >
-        Recipient Address
-      </h3>
-      <h3
-        className={classNames(styles.sendRecipientListHeader, styles.reference)}
-      >
-        Reference
-      </h3>
-      <div className={styles.delete} />
-    </div>
-    <ul className={styles.sendRecipientList}>
-      <SendRecipientListItem />
-    </ul>
-  </section>
-)
+type Props = {
+  sendRowDetails: object
+}
+
+const SendRecipientList = ({ sendRowDetails }: Props) => {
+  const renderRows = () =>
+    Object.keys(sendRowDetails).map(rowId => {
+      const row = sendRowDetails[rowId]
+      return (
+        <SendRecipientListItem
+          key={rowId}
+          asset={row.asset}
+          amount={row.amount}
+          address={row.address}
+          note={row.note}
+          id={rowId}
+        />
+      )
+    })
+
+  return (
+    <section className={styles.sendRecipientListContainer}>
+      <div className={styles.sendRecipientListHeaders}>
+        <h3
+          className={classNames(styles.sendRecipientListHeader, styles.asset)}
+        >
+          Asset
+        </h3>
+        <h3
+          className={classNames(styles.sendRecipientListHeader, styles.amount)}
+        >
+          Amount
+        </h3>
+        <h3
+          className={classNames(styles.sendRecipientListHeader, styles.address)}
+        >
+          Recipient Address
+        </h3>
+        <h3
+          className={classNames(
+            styles.sendRecipientListHeader,
+            styles.reference
+          )}
+        >
+          Reference
+        </h3>
+        <div className={styles.delete} />
+      </div>
+      <ul className={styles.sendRecipientList}>{renderRows()}</ul>
+    </section>
+  )
+}
 
 export default SendRecipientList
--- app/containers/Send/Send.jsx
+++ app/containers/Send/Send.jsx
@@ -7,12 +7,31 @@ import SendPanel from '../../components/Send/SendPanel'
 // import styles from './Send.scss'
 
 export default class Send extends React.Component {
+  state = {
+    sendRowDetails: {
+      1: {
+        asset: 'NEO',
+        amount: 0,
+        address: '',
+        note: ''
+      },
+      2: {
+        asset: 'RPX',
+        amount: 1000,
+        address: '',
+        note: ''
+      }
+    }
+  }
+
   render() {
+    const { numRows, sendRowDetails } = this.state
+
     return (
       <section>
         <SendPageHeader />
         <SendAmountsPanel />
-        <SendPanel />
+        <SendPanel sendRowDetails={sendRowDetails} />
       </section>
     )
   }
