d88915c76b31dabae443c0a2e9ba434f922e7781
Maxwell Lasky <mlasky46@gmail.com>
2018-07-04 23:00:10 -0600

    wires up voteActions to VotingModal

--- app/components/Modals/VotingModal/index.js
+++ app/components/Modals/VotingModal/index.js
@@ -1,1 +1,23 @@
-export { default } from './VotingModal'
+// @flow
+import { compose } from 'recompose'
+import { withCall, withData } from 'spunky'
+
+import VotingModal from './VotingModal'
+import voteActions from '../../../actions/voteActions'
+import withNetworkData from '../../../hocs/withNetworkData'
+import withAuthData from '../../../hocs/withAuthData'
+
+const mapVoteDataToProps = (voteData: Object): {
+  votes: number,
+  validators: any
+} => ({
+  votes: voteData.votes,
+  validators: voteData.validators
+})
+
+export default compose(
+  withNetworkData(),
+  withAuthData(),
+  withCall(voteActions),
+  withData(voteActions, mapVoteDataToProps)
+)(VotingModal)
