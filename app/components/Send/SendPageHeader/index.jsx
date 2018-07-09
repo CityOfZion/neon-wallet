820169daf0fae2ab8d3d44d1e0506034252c38b8
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-09 14:34:17 +0200

    Refactor uneccesary class components to stateless functions

--- app/components/Send/SendPageHeader/index.jsx
+++ app/components/Send/SendPageHeader/index.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import RefreshIcon from '../../../assets/icons/refresh.svg'
 
 import styles from './SendPageHeader.scss'
 
-class SendPageHeader extends Component {
-  render() {
-    return (
-      <section className={styles.sendPageHeader}>
-        <h1 className={styles.sendPageHeading}>Send Assets</h1>
-        <button className={styles.sendPageHeaderRefreshButton} type="button">
-          <RefreshIcon className={styles.sendPageHeaderRefreshIcon} />Refresh
-        </button>
-      </section>
-    )
-  }
-}
+const SendPageHeader = () => (
+  <section className={styles.sendPageHeader}>
+    <h1 className={styles.sendPageHeading}>Send Assets</h1>
+    <button className={styles.sendPageHeaderRefreshButton} type="button">
+      <RefreshIcon className={styles.sendPageHeaderRefreshIcon} />Refresh
+    </button>
+  </section>
+)
 
 export default SendPageHeader
