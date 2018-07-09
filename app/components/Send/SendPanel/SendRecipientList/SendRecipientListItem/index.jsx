4361ff9cb00ffeffa6a33d2745bab1064840ccd4
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-09 15:05:18 +0200

    Refactor colors to use SASS variables

--- app/components/Send/SendPanel/SendRecipientList/SendRecipientListItem/index.jsx
+++ app/components/Send/SendPanel/SendRecipientList/SendRecipientListItem/index.jsx
@@ -77,6 +77,7 @@ class SendRecipientListItem extends Component<Props> {
       note,
       asset,
       errors,
+      max,
       showConfirmSend
     } = this.props
 
@@ -98,6 +99,7 @@ class SendRecipientListItem extends Component<Props> {
     ) : (
       <NumberInput
         value={amount}
+        max={max}
         name="amount"
         onChange={this.handleFieldChange}
         customChangeEvent
