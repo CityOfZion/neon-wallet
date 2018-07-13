849cf4b05e200be66840c3ccc332ad7617d91bd4
Maxwell Lasky <mlasky46@gmail.com>
2018-07-13 08:02:52 -0600

    fixes hot reloading in dev mode

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import 'raf/polyfill'
+import { hot } from 'react-hot-loader'
 import React from 'react'
 import { render } from 'react-dom'
 
@@ -6,5 +7,7 @@ import store from './store/configureStore'
 import Root from './components/Root'
 import './styles/tippy.compiled.global.css'
 import './styles/main.global.scss'
+
+if (module.hot) module.hot.accept()
 
 render(<Root store={store} />, document.getElementById('root'))
