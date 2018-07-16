aec0a0722f99132e3f1f2a728d3bff29bc0a5619
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-16 11:17:42 +0200

    Add flash message to signify add contact success

--- app/components/Modals/AddContactModal/index.js
+++ app/components/Modals/AddContactModal/index.js
@@ -2,6 +2,9 @@
 import { compose } from 'recompose'
 import { withActions } from 'spunky'
 import { trim } from 'lodash'
+import { connect } from 'react-redux'
+
+import { showSuccessNotification } from '../../../modules/notifications'
 
 import AddContactModal from './AddContactModal'
 import { addContactActions } from '../../../actions/contactsActions'
@@ -11,6 +14,16 @@ const mapContactActionsToProps = (actions, props) => ({
     actions.call({ name: trim(name), address: trim(address) })
 })
 
+const mapDispatchToProps = dispatch => ({
+  triggerSuccessNotification(text) {
+    dispatch(showSuccessNotification({ message: text }))
+  }
+})
+
 export default compose(
+  connect(
+    null,
+    mapDispatchToProps
+  ),
   withActions(addContactActions, mapContactActionsToProps)
 )(AddContactModal)
