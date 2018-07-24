d161da690eb9778c00461ddedfd0869d30786159
Maxwell Lasky <mlasky46@gmail.com>
2018-07-24 07:40:25 -0600

    cleans up HomeLayout.jsx

--- app/containers/Home/HomeLayout.jsx
+++ app/containers/Home/HomeLayout.jsx
@@ -9,18 +9,12 @@ type Props = {
   children: React$Node,
   renderNavigation?: Function,
   excludeLogoText?: boolean,
-  excludeHeaderText: boolean,
-  excludeLogo: boolean,
-  headerText: string,
-  headerTextUnderline: boolean
+  headerText: string
 }
 
 export default class HomeLayout extends Component<Props> {
   static defaultProps = {
-    headerText: 'Login',
-    excludeHeaderText: false,
-    excludeLogo: false,
-    headerTextUnderline: false
+    headerText: 'Login'
   }
 
   render = () => {
@@ -28,46 +22,26 @@ export default class HomeLayout extends Component<Props> {
       children,
       renderNavigation,
       excludeLogoText,
-      headerText,
-      excludeHeaderText,
-      excludeLogo,
-      headerTextUnderline
+      headerText
     } = this.props
     return (
       <div id="home" className={styles.homeContainer}>
         <div className={styles.innerHomeContainer}>
           {renderNavigation && renderNavigation()}
-          {!excludeLogo && (
-            <div
-              className={
-                renderNavigation
-                  ? classNames(
-                      styles.logoWithNegativeMargin,
-                      styles.logoContainer
-                    )
-                  : styles.logoContainer
-              }
-            >
-              <img className={styles.logo} src={logo} alt="" />
-              {!excludeLogoText && <h1> NEON </h1>}
-            </div>
-          )}
-          {!excludeHeaderText && (
-            <div
-              style={
-                headerTextUnderline
-                  ? {
-                      borderBottom: 'solid thin rgba(0, 0, 0, 0.15)',
-                      paddingBottom: '20px'
-                    }
-                  : undefined
-              }
-              className={styles.loginHeader}
-            >
-              {' '}
-              {headerText}{' '}
-            </div>
-          )}
+          <div
+            className={
+              renderNavigation
+                ? classNames(
+                    styles.logoWithNegativeMargin,
+                    styles.logoContainer
+                  )
+                : styles.logoContainer
+            }
+          >
+            <img className={styles.logo} src={logo} alt="" />
+            {!excludeLogoText && <h1> NEON </h1>}
+          </div>
+          <div className={styles.loginHeader}>{headerText}</div>
           {children}
         </div>
       </div>
