7dd90db6bfa55d9634f811d816f568d6178f8f1b
Maxwell Lasky <mlasky46@gmail.com>
2018-07-24 08:39:14 -0600

    polishes add contact view

--- app/components/Contacts/AddContactPanel/AddContactPanel.jsx
+++ app/components/Contacts/AddContactPanel/AddContactPanel.jsx
@@ -1,17 +1,12 @@
 // @flow
 import React from 'react'
-import { Link } from 'react-router-dom'
-import classNames from 'classnames'
 import { noop } from 'lodash'
 
 import FullHeightPanel from '../../Panel/FullHeightPanel'
 import ContactForm from '../ContactForm'
-import ArrowIcon from '../../../assets/icons/arrow.svg'
 import { ROUTES } from '../../../core/constants'
 import AddIcon from '../../../assets/icons/add.svg'
 import BackButton from '../../BackButton'
-
-import styles from './AddContactPanel.scss'
 
 type Props = {
   className: ?string,
@@ -32,15 +27,9 @@ export default class AddContactPanel extends React.Component<Props> {
   render() {
     const { className, name, address } = this.props
 
-    // headerText={option === 'CREATE' ? 'Create New Wallet' : 'Import Wallet'}
-    // renderHeaderIcon={() =>
-    //   option === 'IMPORT' ? <AddIcon /> : <AddIcon />
-    // }
-    // renderBackButton={() => <BackButton routeTo={ROUTES.HOME} />}
-
     return (
       <FullHeightPanel
-        className={classNames(styles.addContactPanel, className)}
+        className={className}
         renderHeaderIcon={() => <AddIcon />}
         renderBackButton={() => <BackButton routeTo={ROUTES.CONTACTS} />}
         headerText="Add A Contact"
@@ -56,15 +45,6 @@ export default class AddContactPanel extends React.Component<Props> {
       </FullHeightPanel>
     )
   }
-
-  // renderHeader = () => (
-  //   <span className={styles.header}>
-  //     <Link to={ROUTES.CONTACTS} className={styles.back}>
-  //       <ArrowIcon />
-  //     </Link>
-  //     <span>New Contact</span>
-  //   </span>
-  // )
 
   handleSubmit = (name: string, address: string) => {
     this.props.onSave(name, address)
