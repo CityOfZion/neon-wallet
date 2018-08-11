5865ca0ff1260e1dd652ac38b0c0ee5f03995245
Alex Gray <alexgray248@gmail.com>
2018-08-11 16:15:50 +0100

    creates blank settings panel with text header

--- app/containers/Settings/Settings.jsx
+++ app/containers/Settings/Settings.jsx
@@ -1,20 +1,21 @@
 // @flow
-import React, { Component } from 'react'
-import { map, reject } from 'lodash'
+import React, {Component} from 'react'
+import {map, reject} from 'lodash'
 import fs from 'fs'
 import storage from 'electron-json-storage'
 
 import Delete from 'react-icons/lib/md/delete'
-import { recoverWallet } from '../../modules/generateWallet'
+import {recoverWallet} from '../../modules/generateWallet'
 
 import Button from '../../components/Button'
+import Panel from '../../components/Panel'
 import UnderlinedHeader from '../../components/Headers/UnderlinedHeader'
 import NetworkSwitch from '../../containers/App/Sidebar/NetworkSwitch'
-import { EXPLORERS, MODAL_TYPES, CURRENCIES } from '../../core/constants'
+import {EXPLORERS, MODAL_TYPES, CURRENCIES} from '../../core/constants'
 import themes from '../../themes'
 import styles from './Settings.scss'
 
-const { dialog } = require('electron').remote
+const {dialog} = require('electron').remote
 
 type Props = {
   setAccounts: (Array<Object>) => any,
@@ -41,51 +42,39 @@ export default class Settings extends Component<Props, State> {
   }
 
   saveWalletRecovery = () => {
-    const { showSuccessNotification, showErrorNotification } = this.props
+    const {showSuccessNotification, showErrorNotification} = this.props
 
     storage.get('userWallet', (errorReading, data) => {
       if (errorReading) {
-        showErrorNotification({
-          message: `An error occurred reading wallet file: ${
-            errorReading.message
-          }`
-        })
+        showErrorNotification({message: `An error occurred reading wallet file: ${errorReading.message}`})
         return
       }
       const content = JSON.stringify(data)
-      dialog.showSaveDialog(
-        {
-          filters: [{ name: 'JSON', extensions: ['json'] }]
-        },
-        fileName => {
-          if (fileName === undefined) {
-            return
+      dialog.showSaveDialog({
+        filters: [
+          {
+            name: 'JSON',
+            extensions: ['json']
           }
-          // fileName is a string that contains the path and filename created in the save file dialog.
-          fs.writeFile(fileName, content, errorWriting => {
-            if (errorWriting) {
-              showErrorNotification({
-                message: `An error occurred creating the file: ${
-                  errorWriting.message
-                }`
-              })
-            } else {
-              showSuccessNotification({
-                message: 'The file has been succesfully saved'
-              })
-            }
-          })
+        ]
+      }, fileName => {
+        if (fileName === undefined) {
+          return
         }
-      )
+        // fileName is a string that contains the path and filename created in the save file dialog.
+        fs.writeFile(fileName, content, errorWriting => {
+          if (errorWriting) {
+            showErrorNotification({message: `An error occurred creating the file: ${errorWriting.message}`})
+          } else {
+            showSuccessNotification({message: 'The file has been succesfully saved'})
+          }
+        })
+      })
     })
   }
 
   loadWalletRecovery = () => {
-    const {
-      showSuccessNotification,
-      showErrorNotification,
-      setAccounts
-    } = this.props
+    const {showSuccessNotification, showErrorNotification, setAccounts} = this.props
 
     dialog.showOpenDialog(fileNames => {
       // fileNames is an array that contains all the selected
@@ -95,49 +84,38 @@ export default class Settings extends Component<Props, State> {
       const filepath = fileNames[0]
       fs.readFile(filepath, 'utf-8', (err, data) => {
         if (err) {
-          showErrorNotification({
-            message: `An error occurred reading the file: ${err.message}`
-          })
+          showErrorNotification({message: `An error occurred reading the file: ${err.message}`})
           return
         }
         const walletData = JSON.parse(data)
 
-        recoverWallet(walletData)
-          .then(data => {
-            showSuccessNotification({ message: 'Recovery was successful.' })
-            setAccounts(data.accounts)
-          })
-          .catch(err => {
-            showErrorNotification({
-              message: `An error occurred recovering wallet: ${err.message}`
-            })
-          })
+        recoverWallet(walletData).then(data => {
+          showSuccessNotification({message: 'Recovery was successful.'})
+          setAccounts(data.accounts)
+        }).catch(err => {
+          showErrorNotification({message: `An error occurred recovering wallet: ${err.message}`})
+        })
       })
     })
   }
 
-  updateExplorerSettings = (e: Object) => {
-    const { setBlockExplorer } = this.props
+  updateExplorerSettings = (e : Object) => {
+    const {setBlockExplorer} = this.props
     setBlockExplorer(e.target.value)
   }
 
-  updateCurrencySettings = (e: Object) => {
-    const { setCurrency } = this.props
+  updateCurrencySettings = (e : Object) => {
+    const {setCurrency} = this.props
     setCurrency(e.target.value)
   }
 
-  updateThemeSettings = (e: Object) => {
-    const { setTheme } = this.props
+  updateThemeSettings = (e : Object) => {
+    const {setTheme} = this.props
     setTheme(e.target.value)
   }
 
-  deleteWalletAccount = (label: string, key: string) => {
-    const {
-      showSuccessNotification,
-      showErrorNotification,
-      setAccounts,
-      showModal
-    } = this.props
+  deleteWalletAccount = (label : string, key : string) => {
+    const {showSuccessNotification, showErrorNotification, setAccounts, showModal} = this.props
 
     showModal(MODAL_TYPES.CONFIRM, {
       title: 'Confirm Delete',
@@ -145,27 +123,17 @@ export default class Settings extends Component<Props, State> {
       onClick: () => {
         storage.get('userWallet', (readError, data) => {
           if (readError) {
-            showErrorNotification({
-              message: `An error occurred reading previously stored wallet: ${
-                readError.message
-              }`
-            })
+            showErrorNotification({message: `An error occurred reading previously stored wallet: ${readError.message}`})
             return
           }
 
-          data.accounts = reject(data.accounts, { key })
+          data.accounts = reject(data.accounts, {key})
 
           storage.set('userWallet', data, saveError => {
             if (saveError) {
-              showErrorNotification({
-                message: `An error occurred updating the wallet: ${
-                  saveError.message
-                }`
-              })
+              showErrorNotification({message: `An error occurred updating the wallet: ${saveError.message}`})
             } else {
-              showSuccessNotification({
-                message: 'Account deletion was successful.'
-              })
+              showSuccessNotification({message: 'Account deletion was successful.'})
               setAccounts(data.accounts)
             }
           })
@@ -179,81 +147,18 @@ export default class Settings extends Component<Props, State> {
   }
 
   render() {
-    const { accounts, explorer, currency, theme } = this.props
+    const {accounts, explorer, currency, theme} = this.props
 
-    return (
-      <section className={styles.settingsContainer}>
-        <UnderlinedHeader text="Settings">
-          <NetworkSwitch />
-          <div>Add token</div>
-        </UnderlinedHeader>
-        <div id="settings">
-          <div className="description">
-            Manage your Neon wallet accounts and settings
-          </div>
-          <div className="settingsForm">
-            <div className="settingsItem">
-              <div className="itemTitle">Tokens</div>
-              <Button onClick={this.openTokenModal}>Manage Tokens</Button>
-            </div>
-            <div className="settingsItem">
-              <div className="itemTitle">Theme</div>
-              <select value={theme} onChange={this.updateThemeSettings}>
-                {Object.keys(themes).map((themeName: string) => (
-                  <option value={themeName} key={themeName}>
-                    {themeName}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="settingsItem">
-              <div className="itemTitle">Block Explorer</div>
-              <select value={explorer} onChange={this.updateExplorerSettings}>
-                {Object.keys(EXPLORERS).map((explorer: ExplorerType) => (
-                  <option key={explorer} value={EXPLORERS[explorer]}>
-                    {EXPLORERS[explorer]}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="settingsItem">
-              <div className="itemTitle">Currency</div>
-              <select value={currency} onChange={this.updateCurrencySettings}>
-                {Object.keys(CURRENCIES).map((currencyCode: string) => (
-                  <option value={currencyCode} key={currencyCode}>
-                    {currencyCode.toUpperCase()}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="settingsItem">
-              <div className="itemTitle">Saved Wallet Accounts</div>
-              {map(accounts, account => (
-                <div className="walletList" key={`wallet${account.key}`}>
-                  <div className="walletItem">
-                    <div className="walletName">{account.key.slice(0, 20)}</div>
-                    <div className="walletKey">{account.label}</div>
-                    <div
-                      className="deleteWallet"
-                      onClick={() =>
-                        this.deleteWalletAccount(account.label, account.key)
-                      }
-                    >
-                      <Delete />
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <Button onClick={this.saveWalletRecovery}>
-              Export wallet recovery file
-            </Button>
-            <Button onClick={this.loadWalletRecovery}>
-              Load wallet recovery file
-            </Button>
-          </div>
-        </div>
-      </section>
-    )
+    return (<section className={styles.settingsContainer}>
+      <UnderlinedHeader text="Settings">
+        <NetworkSwitch/>
+        <div>Add token</div>
+      </UnderlinedHeader>
+      <Panel className={styles.settingsPanel} renderHeader={this.renderHeader}></Panel>
+    </section>)
   }
+
+  renderHeader = () => (<div>
+    <span>Manage your neon wallet</span>
+  </div>)
 }
