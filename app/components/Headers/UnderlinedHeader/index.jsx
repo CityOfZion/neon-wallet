82598422d4c8cb6546e122c861848c3818e74ff9
Alex Gray <alexgray248@gmail.com>
2018-08-11 14:47:49 +0100

    Adds child node & child class name props to underline header

--- app/components/Headers/UnderlinedHeader/index.jsx
+++ app/components/Headers/UnderlinedHeader/index.jsx
@@ -7,12 +7,15 @@ import styles from './UnderlinedHeader.scss'
 
 type Props = {
   className?: string,
+  childClassName?: string,
   text?: string,
+  children: React$Node
 }
 
-const UnderlinedHeader = ({ text, className }: Props) => (
+const UnderlinedHeader = ({ className, childClassName, text, children }: Props) => (
     <section className={classNames(styles.underlinedHeader, className)}>
-      <h1 className={styles.underlinedHeading}>{text}</h1>
+      <h1 className={classNames(styles.underlinedHeading, childClassName)}>{text}</h1>
+      {children}
     </section>
 )
 
