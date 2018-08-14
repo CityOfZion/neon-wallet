8a53ad29bd32dc2967c24c5395f6739bf0537d43
Maxwell Lasky <mlasky46@gmail.com>
2018-08-14 08:33:49 -0600

    updates hardcoded token list and simplifies dynamic fetch logic

--- app/core/nep5.js
+++ app/core/nep5.js
@@ -7,7 +7,6 @@ import { COIN_DECIMAL_LENGTH } from './formatters'
 import { TOKENS, TOKENS_TEST, MAIN_NETWORK_ID, TEST_NETWORK_ID } from './constants'
 
 let fetchedTokens
-let requestToFetchTokensSuccessful = false
 
 export const adjustDecimalAmountForTokenTransfer = (value: string): string =>
   toBigNumber(value).times(10 ** COIN_DECIMAL_LENGTH).round().toNumber()
@@ -31,22 +30,16 @@ export const getDefaultTokens = async (): Promise<Array<TokenItemType>> => {
   if (!fetchedTokens) {
     axios.get('https://raw.githubusercontent.com/CityOfZion/neo-tokens/master/tokenList.json')
       .then(response => {
-        requestToFetchTokensSuccessful = true
-        fetchedTokens = response
+        fetchedTokens = response.data
       })
       .catch(error => {
-        // handle error
-        console.log(error)
+        console.error('Falling back to hardcoded list of NEP5 tokens!', error)
+        // if request to gh fails use hardcoded list
         fetchedTokens = TOKENS
       })
   }
 
-  if (!requestToFetchTokensSuccessful) {
-    tokens.push(...map(TOKENS, (scriptHash, symbol) => getTokenEntry(symbol, scriptHash, MAIN_NETWORK_ID)))
-  } else {
-    tokens.push(...map(fetchedTokens, (tokenData) => getTokenEntry(tokenData.symbol, tokenData.networks['1'].hash, MAIN_NETWORK_ID)))
-  }
-
+  tokens.push(...map(fetchedTokens, (tokenData) => getTokenEntry(tokenData.symbol, tokenData.networks['1'].hash, MAIN_NETWORK_ID)))
   tokens.push(...map(TOKENS_TEST, (scriptHash, symbol) => getTokenEntry(symbol, scriptHash, TEST_NETWORK_ID)))
 
   return tokens
