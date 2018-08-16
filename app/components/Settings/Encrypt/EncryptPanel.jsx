6281a26bc8cac1e37cbf1e124032957f7b2d9dbe
Alex Gray <alexgray248@gmail.com>
2018-08-16 19:22:34 +0100

    adds header to encrypt key panel

--- app/components/Settings/Encrypt/EncryptPanel.jsx
+++ app/components/Settings/Encrypt/EncryptPanel.jsx
@@ -1,8 +1,11 @@
 // @flow
 import React from 'react'
 import classNames from 'classnames'
+import { ROUTES } from '../../../core/constants'
 
 import FullHeightPanel from '../../Panel/FullHeightPanel'
+import ImportIcon from '../../../assets/icons/import.svg'
+import CloseButton from '../../CloseButton'
 import styles from './EncryptPanel.scss'
 
 export default class EncryptPanel extends React.Component<Props> {
@@ -14,9 +17,20 @@ export default class EncryptPanel extends React.Component<Props> {
         className={classNames(styles.encryptPanel, this.props.className)}
         contentClassName={styles.content}
         renderHeader={this.renderHeader}
+        headerText="Encrypt a key"
+        renderCloseButton={() => <CloseButton routeTo={ROUTES.SETTINGS} />}
+        renderHeaderIcon={this.renderIcon}
+        instructions="Choose a passphrase to encrypt an existing key"
       />
     )
   }
 
   renderHeader = () => <span>{this.props.title}</span>
+
+  // TODO change to correct icon
+  renderIcon = () => (
+    <div>
+      <ImportIcon />
+    </div>
+  )
 }
