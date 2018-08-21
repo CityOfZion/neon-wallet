999ca210ba1e376064bfc30782b2f492eeda3add
Fredrik Strand Oseberg <fredrik.no@gmail.com>
2018-08-21 22:46:30 +0200

    feat: start on TokenSaleConfirm UI: WIP

--- app/containers/TokenSale/TokenSale.jsx
+++ app/containers/TokenSale/TokenSale.jsx
@@ -1,27 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
 import SendAmountsPanel from '../../components/Send/SendAmountsPanel'
 import TokenSaleHeader from '../../components/TokenSale/TokenSaleHeader'
 import TokenSalePanel from '../../components/TokenSale/TokenSalePanel'
+import TokenSaleConfirm from '../../components/TokenSale/TokenSaleConfirm'
 
 class TokenSale extends Component {
+  state = {
+    step: 'purchase'
+  }
+
+  setStep = step => this.setState({ step })
+
+  renderPurchase = () => (
+    <section>
+      {' '}
+      <TokenSaleHeader />
+      <SendAmountsPanel
+        currencyCode="usd"
+        sendAmountsData={[
+          {
+            symbol: 'NEO',
+            totalBalance: 3,
+            price: 40,
+            currentBalance: 3,
+            totalBalanceWorth: 40 * 3,
+            remainingBalanceWorth: 40 * 3
+          }
+        ]}
+      />
+      <TokenSalePanel setStep={this.setStep} />
+    </section>
+  )
+
+  renderConfirm = () => <TokenSaleConfirm />
+
   render() {
+    const { step } = this.state
+    const displayTokenSalePurchase = step === 'purchase'
+    const displayTokenSaleConfirm = step === 'confirm'
+
     return (
       <section>
-        <TokenSaleHeader />
-        <SendAmountsPanel
-          currencyCode="usd"
-          sendAmountsData={[
-            {
-              symbol: 'NEO',
-              totalBalance: 3,
-              price: 40,
-              currentBalance: 3,
-              totalBalanceWorth: 40 * 3,
-              remainingBalanceWorth: 40 * 3
-            }
-          ]}
-        />
-        <TokenSalePanel />
+        {displayTokenSalePurchase && this.renderPurchase()}
+        {displayTokenSaleConfirm && this.renderConfirm()}
       </section>
     )
   }
