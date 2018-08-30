5bfc6f643cd3d3ad330a9ed1c71789f5c08f8d26
Alex Gray <alexgray248@gmail.com>
2018-08-30 19:44:09 +0100

    removes sort from node select header

--- app/containers/NodeSelect/NodeSelect.jsx
+++ app/containers/NodeSelect/NodeSelect.jsx
@@ -22,21 +22,13 @@ type Node = {
 }
 
 type Props = {
-  setSort: Function,
-  sort: string,
-  nodesShown: int,
   nodes: Node[]
-}
-
-const SORT_BY_LATENCY: { [key: Sort]: string } = {
-  highToLow: 'Latency high > low',
-  lowToHigh: 'Latency low > high'
 }
 
 export default class NodeSelect extends React.Component<Props, State> {
   render() {
-    const { nodesShown } = this.props
-
+    const { nodes } = this.props
+    const count = nodes ? nodes.length : 0
     return (
       <FullHeightPanel
         className={styles.nodeSelectPanel}
@@ -48,14 +40,7 @@ export default class NodeSelect extends React.Component<Props, State> {
       >
         <section className={styles.tableContainer}>
           <div className={styles.header}>
-            <div className={styles.sortContainer}>
-              <span className={styles.label}>Sorting</span>
-              <span className={styles.sort} onClick={this.handleChangeSort}>
-                {this.getSort()}
-                <DropdownIcon className={styles.icon} />
-              </span>
-            </div>
-            <div className={styles.count}>Top {nodesShown} nodes listed</div>
+            <div className={styles.count}>Top {count} nodes listed</div>
             <div className={styles.automaticSelect}>
               <ConfirmIcon className={styles.icon} />
               <span>Select automatically</span>
@@ -115,16 +100,4 @@ export default class NodeSelect extends React.Component<Props, State> {
       below
     </div>
   )
-
-  handleChangeSort = () => {
-    const { sort } = this.props
-    const sortOptions = keys(SORT_BY_LATENCY)
-    const index = (sortOptions.indexOf(sort) + 1) % sortOptions.length
-    this.props.setSort(sortOptions[index])
-  }
-
-  getSort = () => {
-    const { sort } = this.props
-    return SORT_BY_LATENCY[sort]
-  }
 }
