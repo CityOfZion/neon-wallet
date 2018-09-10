58c9cf7d9d7f0aa84ecd8980a8135282e9a5a144
Maxwell Lasky <mlasky46@gmail.com>
2018-09-09 21:10:04 -0600

    wires notifications to tx history container

--- app/containers/TransactionHistory/index.js
+++ app/containers/TransactionHistory/index.js
@@ -6,9 +6,22 @@ import TransactionHistory from './TransactionHistory'
 import transactionHistoryActions from '../../actions/transactionHistoryActions'
 import withNetworkData from '../../hocs/withNetworkData'
 import withAuthData from '../../hocs/withAuthData'
+import balancesActions from '../../actions/balancesActions'
+import withLoadingProp from '../../hocs/withLoadingProp'
+import withSuccessNotification from '../../hocs/withSuccessNotification'
+import withFailureNotification from '../../hocs/withFailureNotification'
 
 export default compose(
   withNetworkData(),
   withAuthData(),
+  withLoadingProp(balancesActions),
+  withSuccessNotification(
+    balancesActions,
+    'Received latest blockchain information.'
+  ),
+  withFailureNotification(
+    balancesActions,
+    'Failed to retrieve blockchain information.'
+  ),
   withCall(transactionHistoryActions)
 )(TransactionHistory)
