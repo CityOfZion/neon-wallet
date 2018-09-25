f410d3bcef66d7c79c0c46c743e4609b08d0c349
Maxwell Lasky <mlasky46@gmail.com>
2018-09-25 10:25:54 -0600

    updates to contact design

--- app/components/Contacts/ContactsPanel/ContactsPanel.jsx
+++ app/components/Contacts/ContactsPanel/ContactsPanel.jsx
@@ -2,8 +2,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { map, isEmpty } from 'lodash-es'
+import classNames from 'classnames'
 
+import StyledReactSelect from '../../Inputs/StyledReactSelect/StyledReactSelect'
 import HeaderBar from '../../HeaderBar'
+import Address from '../../Blockchain/Address'
 import Panel from '../../Panel'
 import Button from '../../Button'
 import AddIcon from '../../../assets/icons/add.svg'
@@ -24,8 +27,37 @@ type Props = {
   deleteContact: Function
 }
 
-export default class ContactsPanel extends React.Component<Props> {
+type State = {
+  sorting: {
+    label: string,
+    value: string
+  }
+}
+
+type SelectOption = {
+  label: string,
+  value: string
+}
+
+const SORTING_OPTIONS = [
+  {
+    label: 'Sorting A-Z',
+    value: 'DESC'
+  },
+  {
+    label: 'Sorting Z-A',
+    value: 'ASC'
+  }
+]
+
+export default class ContactsPanel extends React.Component<Props, State> {
+  state = {
+    sorting: SORTING_OPTIONS[0]
+  }
+
   render() {
+    const { contacts } = this.props
+    console.log(contacts)
     return (
       <React.Fragment>
         <HeaderBar
@@ -41,13 +73,16 @@ export default class ContactsPanel extends React.Component<Props> {
         <Panel
           className={styles.contactsPanel}
           renderHeader={this.renderHeader}
+          contentClassName={styles.contactPanelContent}
         >
-          {!isEmpty(this.props.contacts) && (
+          {!isEmpty(contacts) && (
             <div className={styles.contacts}>
-              {map(this.props.contacts, this.renderContact)}
+              {Object.keys(contacts).map((key, i) =>
+                this.renderContact({ address: contacts[key], name: key }, i)
+              )}
             </div>
           )}
-          {isEmpty(this.props.contacts) && (
+          {isEmpty(contacts) && (
             <div className={styles.emptyContactsContainer}>
               <LogoWithStrikethrough />
             </div>
@@ -57,48 +92,80 @@ export default class ContactsPanel extends React.Component<Props> {
     )
   }
 
-  renderHeader = () => (
-    <div className={styles.header}>
-      <span>Contacts</span>
-    </div>
-  )
-
-  renderContact = (address: string, name: string) => (
-    <div key={name} className={styles.contact}>
-      <div className={styles.name}>{name}</div>
-      <div className={styles.address}>
-        {address}{' '}
-        <CopyToClipboard
-          className={styles.copy}
-          text={address}
-          tooltip="Copy Public Address"
+  renderHeader = () => {
+    const { sorting } = this.state
+    const { contacts } = this.props
+    return (
+      // <div className={styles.header}>
+      //   <span>Contacts</span>
+      // </div>
+      <div className={styles.headerSelect}>
+        <StyledReactSelect
+          disabled={isEmpty(contacts)}
+          value={sorting}
+          onChange={this.handleSort}
+          options={SORTING_OPTIONS}
+          isSearchable={false}
+          hideHighlight
         />
       </div>
-      <div className={styles.actions}>
-        <Button
-          className={styles.editButton}
-          renderIcon={EditIcon}
-          onClick={this.handleEdit(name)}
-        >
-          Edit
-        </Button>
-        <Button
-          className={styles.infoButton}
-          renderIcon={InfoIcon}
-          onClick={this.handleEdit(name)}
-        >
-          View Activity
-        </Button>
-        <Button
-          className={styles.sendButton}
-          renderIcon={SendIcon}
-          onClick={this.handleEdit(name)}
-        >
-          Send Assets
-        </Button>
+    )
+  }
+
+  renderContact = (contact: Object, i: number) => {
+    const { name, address } = contact
+    return (
+      <div
+        key={name}
+        className={classNames(styles.contact, {
+          [styles.oddNumberedRow]: i % 2 === 0
+        })}
+      >
+        <div className={styles.name}>{name}</div>
+        <div className={styles.address}>
+          {address}{' '}
+          <CopyToClipboard
+            className={styles.copy}
+            text={address}
+            tooltip="Copy Public Address"
+          />
+        </div>
+        <div className={styles.actions}>
+          <Button
+            className={styles.editButton}
+            renderIcon={EditIcon}
+            onClick={this.handleEdit(name)}
+          >
+            Edit
+          </Button>
+          <Address address={address} asWrapper>
+            <Button className={styles.infoButton} renderIcon={InfoIcon}>
+              View Activity
+            </Button>
+          </Address>
+          <Link
+            to={{
+              pathname: ROUTES.SEND,
+              state: { address }
+            }}
+            className={styles.settingsDonations}
+          >
+            <Button
+              className={styles.sendButton}
+              renderIcon={SendIcon}
+              // onClick={this.handleEdit(name)}
+            >
+              Send Assets
+            </Button>
+          </Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  handleSort = (option: SelectOption) => {
+    this.setState({ sorting: option })
+  }
 
   handleEdit = (name: string) => () => {
     this.props.history.push(`/contacts/edit/${encodeURIComponent(name)}`)
