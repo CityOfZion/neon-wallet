28ecf4237e1d20631c3f80365585c80316201a3e
Evgeny Boxer <evgeny.boxer@gmail.com>
2018-10-09 09:58:13 +1100

    feat(login): Implement a more intuitive login flow with ledger (#1493)
    * implement a more intuitive login flow with ledger
    
    * rename method
    
    * remove !important
    
    * rename classes
    
    * change method to a better name for readability

--- app/containers/LoginLedgerNanoS/LoginLedgerNanoS.jsx
+++ app/containers/LoginLedgerNanoS/LoginLedgerNanoS.jsx
@@ -1,35 +1,80 @@
 // @flow
 import React from 'react'
 import { progressValues } from 'spunky'
+import classNames from 'classnames'
 
 import Button from '../../components/Button'
 import LoginIcon from '../../assets/icons/login.svg'
+import ConfirmIcon from '../../assets/icons/confirm.svg'
+import RefreshIcon from '../../assets/icons/refresh.svg'
 import styles from '../Home/Home.scss'
+
+import { MESSAGES } from '../../ledger/neonLedger'
+
+const LEDGER_CONNECTION_STAGES = {
+  NOT_CONNECTED: 1,
+  OPEN_APP: 2,
+  CONNECTED: 3
+}
 
 const { LOADED, FAILED } = progressValues
 
-type DeviceInfo = {
-  manufacturer: string,
-  product: string
-}
+const { NOT_CONNECTED, OPEN_APP, CONNECTED } = LEDGER_CONNECTION_STAGES
+
+type LedgerConnectionStage = $Values<typeof LEDGER_CONNECTION_STAGES>
 
 type Props = {
   progress: string,
   login: Function,
   connect: Function,
-  deviceInfo: ?DeviceInfo,
   publicKey: ?string,
   error: ?string
 }
 
-const POLL_FREQUENCY = 1000
+type State = {
+  ledgerStage: LedgerConnectionStage,
+  isLoading: boolean
+}
 
-export default class LoginLedgerNanoS extends React.Component<Props> {
+const POLL_FREQUENCY_MS = 1000
+
+export default class LoginLedgerNanoS extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = this.computeStateFromProps(props)
+  }
+
   intervalId: ?number
 
   componentDidMount() {
     // $FlowFixMe
-    this.intervalId = setInterval(this.props.connect, POLL_FREQUENCY)
+    this.intervalId = setInterval(this.props.connect, POLL_FREQUENCY_MS)
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    const { progress, error } = this.props
+
+    if (progress !== nextProps.progress || error !== nextProps.error) {
+      this.setState(this.computeStateFromProps(nextProps))
+    }
+  }
+
+  computeStateFromProps = (props: Props) => {
+    if (props.progress === LOADED) {
+      return { ledgerStage: CONNECTED, isLoading: false }
+    }
+    if (props.progress === FAILED && props.error) {
+      return {
+        isLoading: true,
+        ledgerStage:
+          props.error === MESSAGES.APP_CLOSED ? OPEN_APP : NOT_CONNECTED
+      }
+    }
+    return {
+      isLoading: true,
+      ledgerStage: NOT_CONNECTED
+    }
   }
 
   componentWillUnmount() {
@@ -61,26 +106,43 @@ export default class LoginLedgerNanoS extends React.Component<Props> {
     )
   }
 
+  getStatusIcon = (ledgerStage: LedgerConnectionStage) => {
+    if (this.state.isLoading && this.state.ledgerStage === ledgerStage) {
+      return <RefreshIcon className={styles.ledgerStageRefreshIcon} />
+    }
+    if (this.state.ledgerStage > ledgerStage) {
+      return <ConfirmIcon className={styles.ledgerStageIcon} />
+    }
+    return <i />
+  }
+
   renderStatus = () => {
-    const { progress, deviceInfo, error } = this.props
-
-    if (progress === LOADED && deviceInfo) {
-      return (
-        <p className={styles.ledgerStatusText}>
-          Found USB {deviceInfo.manufacturer} {deviceInfo.product}. NEO app
-          found on hardward device. Click button above to login.
-        </p>
-      )
-    }
-
-    if (progress === FAILED && error) {
-      return <p className={styles.ledgerStatusTextError}>{error}</p>
-    }
-
+    const { ledgerStage } = this.state
     return (
-      <p className={styles.ledgerStatusText}>
-        Looking for USB Devices. Please plugin your device and login.
-      </p>
+      <div>
+        <div
+          className={classNames(styles.ledgerStage, {
+            [styles.ledgerStageActive]: ledgerStage === NOT_CONNECTED,
+            [styles.ledgerStageCompleted]: ledgerStage > NOT_CONNECTED
+          })}
+        >
+          {this.getStatusIcon(NOT_CONNECTED)}
+          <div>
+            Connect and unlock your <strong>Ledger device</strong>
+          </div>
+        </div>
+        <div
+          className={classNames(styles.ledgerStage, {
+            [styles.ledgerStageActive]: ledgerStage === OPEN_APP,
+            [styles.ledgerStageCompleted]: ledgerStage > OPEN_APP
+          })}
+        >
+          {this.getStatusIcon(OPEN_APP)}
+          <div>
+            Navigate to the <strong>NEO app</strong> on your device
+          </div>
+        </div>
+      </div>
     )
   }
 
