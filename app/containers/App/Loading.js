b481a772046bdbf8078ff3f405fa7ee369149d8c
Maxwell Lasky <mlasky46@gmail.com>
2018-10-21 09:13:45 -0600

    cleans up broken styles introduced by theming

--- app/containers/App/Loading.js
+++ app/containers/App/Loading.js
@@ -1,11 +1,13 @@
+// @flow
 import React from 'react'
 
+import themes from '../../themes'
 import Loader from '../../components/Loader'
 import styles from './Loading.scss'
 
-export default function Loading() {
+export default function Loading({ theme }: { theme: ThemeType }) {
   return (
-    <div className={styles.loading}>
+    <div style={themes[theme]} className={styles.loading}>
       <Loader />
     </div>
   )
