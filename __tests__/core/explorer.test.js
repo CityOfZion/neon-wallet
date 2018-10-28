37e66c7209d4c52887e927f93a82cbd24a901677
Evgeny Boxer <evgenyboxer@Evgenys-MacBook-Pro-3.local>
2018-10-29 10:02:43 +1100

    fixes antchain explorer tx link + cleanups

--- __tests__/core/explorer.test.js
+++ __tests__/core/explorer.test.js
@@ -1,4 +1,8 @@
-import { getExplorerTxLink, openExplorerTx } from '../../app/core/explorer'
+import {
+  getExplorerAddressLink,
+  getExplorerTxLink,
+  openExplorerTx
+} from '../../app/core/explorer'
 import {
   MAIN_NETWORK_ID,
   TEST_NETWORK_ID,
@@ -7,53 +11,58 @@ import {
 import { shell } from 'electron'
 
 describe('explorer tests', () => {
+  const txId =
+    '33e6dad17f65564222d697923558792feda7847644f1810e6b389106335a7de0'
+  const address = 'AQpLnwMpnhxroPM4fcYGenB2pH5cLhMDao'
+
   describe('getExplorerTxLink tests', () => {
     test('NeoTracker mainnet explorer test', () => {
       const networkId = MAIN_NETWORK_ID
       const explorer = EXPLORERS.NEO_TRACKER
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'https://neotracker.io/tx/1234567890abcdef'
+      const expectedUrl = `https://neotracker.io/tx/${txId}`
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
 
     test('NeoTracker testnet explorer test', () => {
       const networkId = TEST_NETWORK_ID
       const explorer = EXPLORERS.NEO_TRACKER
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'https://testnet.neotracker.io/tx/1234567890abcdef'
+      const expectedUrl = `https://testnet.neotracker.io/tx/${txId}`
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
 
     test('NeoScan mainnet explorer test', () => {
       const networkId = MAIN_NETWORK_ID
       const explorer = EXPLORERS.NEO_SCAN
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'https://neoscan.io/transaction/1234567890abcdef'
+      const expectedUrl = `https://neoscan.io/transaction/${txId}`
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
 
     test('NeoScan testnet explorer test', () => {
       const networkId = TEST_NETWORK_ID
       const explorer = EXPLORERS.NEO_SCAN
-      const txId = '1234567890abcdef'
-      const expectedUrl =
-        'https://neoscan-testnet.io/transaction/1234567890abcdef'
+      const expectedUrl = `https://neoscan-testnet.io/transaction/${txId}`
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
 
     test('AntChain mainnet explorer test', () => {
       const networkId = MAIN_NETWORK_ID
       const explorer = EXPLORERS.ANT_CHAIN
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'http://antcha.in/tx/hash/1234567890abcdef'
+      const expectedUrl =
+        'http://antcha.in/tx/hash/0x33e6dad17f65564222d697923558792feda7847644f1810e6b389106335a7de0'
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
 
     test('AntChain testnet explorer test', () => {
       const networkId = TEST_NETWORK_ID
       const explorer = EXPLORERS.ANT_CHAIN
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'http://testnet.antcha.in/tx/hash/1234567890abcdef'
+      const expectedUrl =
+        'http://testnet.antcha.in/tx/hash/0x33e6dad17f65564222d697923558792feda7847644f1810e6b389106335a7de0'
+
       expect(getExplorerTxLink(networkId, explorer, txId)).toEqual(expectedUrl)
     })
   })
@@ -62,12 +71,74 @@ describe('explorer tests', () => {
     test('open NeoTracker mainnet explorer test', () => {
       const networkId = MAIN_NETWORK_ID
       const explorer = EXPLORERS.NEO_TRACKER
-      const txId = '1234567890abcdef'
-      const expectedUrl = 'https://neotracker.io/tx/1234567890abcdef'
+      const expectedUrl = `https://neotracker.io/tx/${txId}`
       const spy = jest.spyOn(shell, 'openExternal')
 
       openExplorerTx(networkId, explorer, txId)
+
       expect(spy).toHaveBeenCalledWith(expectedUrl)
+    })
+  })
+
+  describe('getExplorerAddressLink tests', () => {
+    test('NeoTracker mainnet explorer test', () => {
+      const networkId = MAIN_NETWORK_ID
+      const explorer = EXPLORERS.NEO_TRACKER
+      const expectedUrl = `https://neotracker.io/address/${address}`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
+    })
+
+    test('NeoTracker testnet explorer test', () => {
+      const networkId = TEST_NETWORK_ID
+      const explorer = EXPLORERS.NEO_TRACKER
+      const expectedUrl = `https://testnet.neotracker.io/address/${address}`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
+    })
+
+    test('NeoScan mainnet explorer test', () => {
+      const networkId = MAIN_NETWORK_ID
+      const explorer = EXPLORERS.NEO_SCAN
+      const expectedUrl = `https://neoscan.io/address/${address}/1`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
+    })
+
+    test('NeoScan testnet explorer test', () => {
+      const networkId = TEST_NETWORK_ID
+      const explorer = EXPLORERS.NEO_SCAN
+      const expectedUrl = `https://neoscan-testnet.io/address/${address}/1`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
+    })
+
+    test('AntChain mainnet explorer test', () => {
+      const networkId = MAIN_NETWORK_ID
+      const explorer = EXPLORERS.ANT_CHAIN
+      const expectedUrl = `http://antcha.in/address/info/${address}`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
+    })
+
+    test('AntChain testnet explorer test', () => {
+      const networkId = TEST_NETWORK_ID
+      const explorer = EXPLORERS.ANT_CHAIN
+      const expectedUrl = `http://testnet.antcha.in/address/info/${address}`
+
+      expect(getExplorerAddressLink(networkId, explorer, address)).toEqual(
+        expectedUrl
+      )
     })
   })
 })
