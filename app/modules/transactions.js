407005e4b3d916e329a0bc4b7f9420b2f4256b98
Maxwell Lasky <mlasky46@gmail.com>
2018-10-30 22:37:16 -0600

    removes console log in transactions.js

--- app/modules/transactions.js
+++ app/modules/transactions.js
@@ -118,8 +118,6 @@ export const sendTransaction = ({
 
     const error = validateTransactionsBeforeSending(balances, sendEntries)
 
-    console.log({ sendEntries })
-
     if (error) {
       rejectTransaction(error)
       return reject(error)
