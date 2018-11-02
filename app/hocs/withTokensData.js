b629b39db962eddb78f6972ae39a05756be9f549
Maxwell Lasky <mlasky46@gmail.com>
2018-11-01 22:39:10 -0600

    reverts changes to app/hocs/withTokensData.js

--- app/hocs/withTokensData.js
+++ app/hocs/withTokensData.js
@@ -1,15 +1,13 @@
 // @flow
-import { withData, withRecall, withReset } from 'spunky'
+import { withData, withRecall } from 'spunky'
 import { compose } from 'recompose'
 
 import settingsActions from '../actions/settingsActions'
 import icoTokensActions from '../actions/icoTokensActions'
-import balancesActions from '../actions/balancesActions'
 
 export default function withTokensData(key: string = 'tokens') {
   return compose(
     withData(settingsActions, settings => ({ [key]: settings.tokens })),
-    withRecall(icoTokensActions, ['tokens']),
-    withReset(balancesActions, ['tokens'])
+    withRecall(icoTokensActions, ['tokens'])
   )
 }
