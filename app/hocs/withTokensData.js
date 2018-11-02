55b1d2f35cc2cbb0101b138b01e379a5367f4bf9
Maxwell Lasky <mlasky46@gmail.com>
2018-11-01 22:31:47 -0600

    adding a token will now update balances

--- app/hocs/withTokensData.js
+++ app/hocs/withTokensData.js
@@ -1,13 +1,15 @@
 // @flow
-import { withData, withRecall } from 'spunky'
+import { withData, withRecall, withReset } from 'spunky'
 import { compose } from 'recompose'
 
 import settingsActions from '../actions/settingsActions'
 import icoTokensActions from '../actions/icoTokensActions'
+import balancesActions from '../actions/balancesActions'
 
 export default function withTokensData(key: string = 'tokens') {
   return compose(
     withData(settingsActions, settings => ({ [key]: settings.tokens })),
-    withRecall(icoTokensActions, ['tokens'])
+    withRecall(icoTokensActions, ['tokens']),
+    withReset(balancesActions, ['tokens'])
   )
 }
