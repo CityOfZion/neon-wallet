00b3c0c23904b3ace7b80438ea9c16da96075d79
Fredrik Strand Oseberg <fredrik.no@gmail.com>
2018-11-08 19:42:07 +0100

    refactor: remove unused props from Receive.jsx

--- app/containers/Receive/Receive.jsx
+++ app/containers/Receive/Receive.jsx
@@ -46,8 +46,6 @@ export default class Receive extends React.Component<Props, State> {
     const {
       sendableAssets,
       currencyCode,
-      loading,
-      loadWalletData,
       address,
       showReceiveModal,
       networkId
