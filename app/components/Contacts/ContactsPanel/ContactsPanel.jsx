eb0bf51020fd9a191586f0b1b4afe7f89810553a
phetter <phetter@users.noreply.github.com>
2019-01-09 19:29:17 -0500

    add delete button to contact list view

--- app/components/Contacts/ContactsPanel/ContactsPanel.jsx
+++ app/components/Contacts/ContactsPanel/ContactsPanel.jsx
@@ -12,8 +12,9 @@ import Button from '../../Button'
 import AddIcon from '../../../assets/icons/add.svg'
 import InfoIcon from '../../../assets/icons/info.svg'
 import EditIcon from '../../../assets/icons/edit.svg'
+import DeleteIcon from '../../../assets/icons/delete.svg'
 import SendIcon from '../../../assets/icons/send.svg'
-import { ROUTES } from '../../../core/constants'
+import { ROUTES, MODAL_TYPES } from '../../../core/constants'
 import CopyToClipboard from '../../CopyToClipboard'
 import LogoWithStrikethrough from '../../LogoWithStrikethrough'
 
@@ -136,6 +137,13 @@ export default class ContactsPanel extends React.Component<Props, State> {
         >
           Edit
         </Button>
+        <Button
+          className={styles.deleteButton}
+          renderIcon={DeleteIcon}
+          onClick={() => this.handleDelete(name)}
+        >
+          Delete
+        </Button>
         <Address address={address} asWrapper>
           <Button className={styles.infoButton} renderIcon={InfoIcon}>
             View Activity
@@ -206,5 +214,20 @@ export default class ContactsPanel extends React.Component<Props, State> {
         </Panel>
       </React.Fragment>
     )
+  }
+
+  handleDelete = (name) => {
+    const { showModal, showSuccessNotification } = this.props
+
+    showModal(MODAL_TYPES.CONFIRM, {
+      title: 'Confirm Delete',
+      text: `Please confirm removing contact - ${name}`,
+      onClick: () => {
+        this.props.deleteContact(name)
+        showSuccessNotification({
+          message: 'Contact removal was successful.',
+        })
+      },
+    })
   }
 }
