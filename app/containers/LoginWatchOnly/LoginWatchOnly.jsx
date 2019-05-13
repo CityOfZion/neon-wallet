4667dd5c67ef29e3d4d0226f900212d030e9e40d
Maxwell Lasky <mlasky46@gmail.com>
2019-05-13 15:35:31 -0600

    Removes unnused imports from app/containers/LoginWatchOnly/LoginWatchOnly.jsx

--- app/containers/LoginWatchOnly/LoginWatchOnly.jsx
+++ app/containers/LoginWatchOnly/LoginWatchOnly.jsx
@@ -1,13 +1,8 @@
 // @flow
 import React from 'react'
-import { type ProgressState } from 'spunky'
 
-import QrCodeScanner from '../../components/QrCodeScanner'
 import Button from '../../components/Button'
-// import PasswordInput from '../../components/Inputs/PasswordInput/PasswordInput'
 import LoginIcon from '../../assets/icons/login.svg'
-import GridIcon from '../../assets/icons/grid.svg'
-import Close from '../../assets/icons/close.svg'
 import styles from '../Home/Home.scss'
 import TextInput from '../../components/Inputs/TextInput'
 
