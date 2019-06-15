2a9b5a4077ea3d965b6994c9d432c3d98dc839ca
DauTT <dautruongtan@gmail.com>
2019-06-15 22:08:47 +0200

    Fix yarn type annotation error

--- app/components/Settings/EncryptQR/index.js
+++ app/components/Settings/EncryptQR/index.js
@@ -1,13 +1,14 @@
 // @flow
 
 import { connect } from 'react-redux'
-import { compose } from 'recompose'
 import { getEncryptedWIF } from '../../../modules/generateEncryptedWIF'
-
 import EncryptQR from './EncryptQR'
 
 const mapStateToProps = (state: Object) => ({
   encryptedWIF: getEncryptedWIF(state),
 })
 
-export default compose(connect(mapStateToProps))(EncryptQR)
+export default connect(
+  mapStateToProps,
+  (dispatch: Dispatch) => ({ dispatch }),
+)(EncryptQR)
