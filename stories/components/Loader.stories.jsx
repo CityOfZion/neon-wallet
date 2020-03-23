5e796c0da082992385a166c6b8883d16881fa635
Maxwell Lasky <mlasky46@gmail.com>
2020-03-22 21:34:53 -0600

    Removes storybook and associated files

--- stories/components/Loader.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-import Loader from 'components/Loader'
-
-storiesOf('Components/Loader', module).add('default', () => <Loader />)
