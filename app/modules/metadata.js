6a1709e040bc4ebc6a5835a3c7c842fe8322bdc9
Maxwell Lasky <mlasky46@gmail.com>
2020-11-10 07:49:01 -0700

    Only render new release notification on Linux

--- app/modules/metadata.js
+++ app/modules/metadata.js
@@ -12,6 +12,9 @@ import {
 } from '../core/constants'
 import { version } from '../../package.json'
 
+const electron = require('electron').remote
+const { app } = electron
+
 const CURRENT_RELEASE_URL =
   'https://api.github.com/repos/CityOfZion/neon-wallet/releases/latest'
 export const RETRY_CONFIG = {
@@ -52,7 +55,20 @@ export const checkVersion = () => async (dispatch: DispatchType) => {
       response.data &&
       compareVersions(version, response.data.tag_name) === -1
 
-    if (shouldUpdate) {
+    let opsys
+    const { platform } = process
+
+    if (platform === 'darwin') {
+      opsys = 'MacOS'
+    } else if (platform === 'win32' || platform === 'win64') {
+      opsys = 'Windows'
+    } else if (platform === 'linux') {
+      opsys = 'Linux'
+    }
+
+    // TODO: this should really only be a check against .deb
+    // file extension as auto updates only work on .AppImage
+    if (shouldUpdate && opsys === 'Linux') {
       showError(
         <div>
           Your wallet is out of date! Please download the latest version from{' '}
