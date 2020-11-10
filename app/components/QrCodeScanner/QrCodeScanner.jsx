2c1fea3e9cb0c5a4532cabaf975d5a3901e06c75
Maxwell Lasky <mlasky46@gmail.com>
2020-11-09 21:45:34 -0700

    Fixes button style in QR scanner

--- app/components/QrCodeScanner/QrCodeScanner.jsx
+++ app/components/QrCodeScanner/QrCodeScanner.jsx
@@ -168,6 +168,7 @@ export default class QrCodeScanner extends Component<Props, State> {
   }
 
   static getScannerError(err: Error) {
+    console.error({ err })
     const scanErr: ScannerError = {
       message: 'Could not connect to camera',
     }
