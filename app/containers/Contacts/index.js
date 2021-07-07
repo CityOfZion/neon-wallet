ff6bde5fbe0fd09d11d1e3f8933d64adb7a40d97
endkeyCoder <helplace2019@gmail.com>
2021-07-07 18:49:59 -0300

    it changed the filter of contacts by chain to action contactActions

--- app/containers/Contacts/index.js
+++ app/containers/Contacts/index.js
@@ -1,9 +1,12 @@
 // @flow
-import { withData } from 'spunky'
-
+import { withCall, withData } from 'spunky'
+import { compose } from 'recompose'
 import Contacts from './Contacts'
 import contactsActions from '../../actions/contactsActions'
 
 const mapContactsDataToProps = (contacts: Object) => ({ contacts })
 
-export default withData(contactsActions, mapContactsDataToProps)(Contacts)
+export default compose(
+  withCall(contactsActions),
+  withData(contactsActions, mapContactsDataToProps),
+)(Contacts)
