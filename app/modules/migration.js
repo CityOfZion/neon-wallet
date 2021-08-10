b465a848b53f26555c53f59f38ee6e3e29c9337f
Maxwell Lasky <mlasky46@gmail.com>
2021-08-10 17:51:17 -0600

    Mint CGAS if below threshold

--- app/modules/migration.js
+++ app/modules/migration.js
@@ -21,6 +21,11 @@ export const performMigration = ({
   // eslint-disable-next-line
   const provider = new N2.api.neoCli.instance('https://testnet1.neo2.coz.io')
 
+  // TODO: Implement the following logic:
+  // Please ensure at least 1 cGAS at the user's address before starting the migration, otherwise the migration may fail!
+  // Currently the cross-chain contract adds a condition that migration to N3 is free as long as the amount of assets is
+  // greater than or equal to 10 NEO or 20 GAS; otherwise, the contract will charge 1 cGAS at the user's address if the condition is not met.
+
   return new Promise(async (resolve, reject) => {
     try {
       const state = getState()
@@ -32,8 +37,7 @@ export const performMigration = ({
       const entry = sendEntries[0]
 
       const { symbol, amount } = entry
-
-      console.log({ symbol })
+      const MINT_REQUIRED = symbol === 'NEO' || symbol === 'GAS'
 
       const mintScript = N2.sc.createScript({
         scriptHash: symbol === 'NEO' ? NNEO : CGAS,
@@ -49,7 +53,7 @@ export const performMigration = ({
           N2.u.reverseHex(FROM_ACCOUNT.scriptHash), // sender on original chain
           '58', // destination chain ID
           N2.u.reverseHex(TO_ACCOUNT.scriptHash), // recipient on new chain
-          symbol === 'NEO' ? amount : Number(amount) * 100000000, // amount
+          Number(amount) * 100000000, // amount
           0, // ?
           0, // ?
         ],
@@ -60,22 +64,43 @@ export const performMigration = ({
       const CONFIG = {
         api: provider,
         account: FROM_ACCOUNT,
-        intents:
-          symbol === 'NEO' || symbol === 'GAS'
-            ? N2.api.makeIntent(
-                {
-                  [symbol]: amount,
-                },
-                N2.wallet.getAddressFromScriptHash(
-                  symbol === 'GAS' || symbol === 'CGAS' ? CGAS : NNEO,
-                ),
-              )
-            : null,
-        script:
-          symbol === 'NEO' || symbol === 'GAS'
-            ? mintScript + swapScript
-            : swapScript,
+        script: MINT_REQUIRED ? mintScript + swapScript : swapScript,
+        intents: undefined,
         gas,
+      }
+
+      if (MINT_REQUIRED) {
+        const mintAmount = {
+          [symbol]: Number(amount),
+        }
+
+        // TODO: none of this is necessary is wallet has > 1 CGAS balance
+
+        // TODO: If wallet has less than 1 GAS and the transaction is below the minimum
+        // we need to throw an error
+
+        if (symbol === 'GAS' && Number(amount) < 20) {
+          // $FlowFixMe
+          mintAmount.GAS += 1
+        }
+
+        const mintIntents = N2.api.makeIntent(
+          mintAmount,
+          N2.wallet.getAddressFromScriptHash(
+            symbol === 'GAS' || symbol === 'CGAS' ? CGAS : NNEO,
+          ),
+        )
+
+        if (symbol === 'NEO' && Number(amount) < 10) {
+          // $FlowFixMe
+          const mintGasFeeIntents = N2.api.makeIntent(
+            1,
+            N2.wallet.getAddressFromScriptHash(CGAS),
+          )
+          CONFIG.intents = mintGasFeeIntents + mintIntents
+        } else {
+          CONFIG.intents = mintIntents
+        }
       }
 
       const config = await N2.api.doInvoke(CONFIG)
