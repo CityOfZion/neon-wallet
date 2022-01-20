262b1e1f5c365cf4b73ed3b10ffe5715e47ed179
Maxwell Lasky <mlasky46@gmail.com>
2022-01-20 13:36:57 -0700

    resolves unnecesary recursion in activity feed action (#2368)
    Co-authored-by: lllwvlvwlll <lllwvlvwlll@gmail.com>

--- app/actions/transactionHistoryActions.js
+++ app/actions/transactionHistoryActions.js
@@ -3,7 +3,10 @@ import { NeoLegacyREST, NeoRest } from '@cityofzion/dora-ts/dist/api'
 import { createActions } from 'spunky'
 import { rpc as n3Rpc, sc, u } from '@cityofzion/neon-js-next'
 import { TX_TYPES } from '../core/constants'
-import { findAndReturnTokenInfo } from '../util/findAndReturnTokenInfo'
+import {
+  getImageBySymbol,
+  findAndReturnTokenInfo,
+} from '../util/findAndReturnTokenInfo'
 import { getSettings } from './settingsActions'
 import { toBigNumber } from '../core/math'
 import { getNode, getRPCEndpoint } from './nodeStorageActions'
@@ -88,57 +91,57 @@ export async function handleNeoActivity(
       let image
       let assets
       let endpoint
-      switch (invocation.type) {
-        case 'nep17_transfer':
-          assets = await findAndReturnTokenInfo(
-            invocation.metadata.scripthash,
-            net,
-          )
-          // eslint-disable-next-line prefer-destructuring
-          image = assets.image
-          break
-        case 'nep11_transfer':
-          // Get the properties of the token
-          endpoint = await getNode(net)
-          if (!endpoint) {
-            endpoint = await getRPCEndpoint(net)
-          }
-          invocation.metadata.tokenName = Buffer.from(
-            invocation.metadata.token_id,
-            'hex',
-          ).toString()
-          assets = await new n3Rpc.RPCClient(endpoint).invokeFunction(
-            invocation.metadata.scripthash,
-            'properties',
-            [sc.ContractParam.string(invocation.metadata.tokenName)],
-          )
-          assets.stack[0].value.some(property => {
-            const key = u.HexString.fromBase64(property.key.value).toAscii()
-            if (key === 'image') {
-              image = u.HexString.fromBase64(property.value.value).toAscii()
-              return true
-            }
-            return false
-          })
-          break
-        default:
-          break
-      }
-      // flatten the invocations into individual events to support existing components
-      invocation.metadata.image = image
-      invocation.hash = item.hash
-      invocation.sender = item.sender
-      invocation.sysfee = item.sysfee
-      invocation.netfee = item.netfee
-      invocation.block = item.block
-      invocation.time = item.time
-      invocation.vmstate = item.vmstate
 
-      if (
-        invocation.metadata.scripthash ===
-        '0xd2a4cff31913016155e38e474a2c06d08be276cf'
-      ) {
-        invocation.metadata.amount /= 10 ** 8
+      try {
+        switch (invocation.type) {
+          case 'nep17_transfer':
+            image = getImageBySymbol(invocation.metadata.symbol)
+            break
+          case 'nep11_transfer':
+            // Get the properties of the token
+            endpoint = await getNode(net)
+            if (!endpoint) {
+              endpoint = await getRPCEndpoint(net)
+            }
+            invocation.metadata.tokenName = Buffer.from(
+              invocation.metadata.token_id,
+              'hex',
+            ).toString()
+            assets = await new n3Rpc.RPCClient(endpoint).invokeFunction(
+              invocation.metadata.scripthash,
+              'properties',
+              [sc.ContractParam.string(invocation.metadata.tokenName)],
+            )
+            assets.stack[0].value.some(property => {
+              const key = u.HexString.fromBase64(property.key.value).toAscii()
+              if (key === 'image') {
+                image = u.HexString.fromBase64(property.value.value).toAscii()
+                return true
+              }
+              return false
+            })
+            break
+          default:
+            break
+        }
+        // flatten the invocations into individual events to support existing components
+        invocation.metadata.image = image
+        invocation.hash = item.hash
+        invocation.sender = item.sender
+        invocation.sysfee = item.sysfee
+        invocation.netfee = item.netfee
+        invocation.block = item.block
+        invocation.time = item.time
+        invocation.vmstate = item.vmstate
+
+        if (
+          invocation.metadata.scripthash ===
+          '0xd2a4cff31913016155e38e474a2c06d08be276cf'
+        ) {
+          invocation.metadata.amount /= 10 ** 8
+        }
+      } catch (e) {
+        console.warn('invocation error:', invocation, e)
       }
       return invocation
     })
