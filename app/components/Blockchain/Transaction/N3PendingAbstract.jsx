301450347af2df58933dce84a17dce7eb43e57c7
Maxwell Lasky <mlasky46@gmail.com>
2022-01-27 11:41:24 -0700

    lint

--- app/components/Blockchain/Transaction/N3PendingAbstract.jsx
+++ app/components/Blockchain/Transaction/N3PendingAbstract.jsx
@@ -1,14 +1,11 @@
 // @flow
 import React, { Fragment } from 'react'
-import { FormattedMessage, injectIntl, IntlShape } from 'react-intl'
+import { injectIntl } from 'react-intl'
+import classNames from 'classnames'
 
-import Button from '../../Button'
 import styles from './Transaction.scss'
 import SendIcon from '../../../assets/icons/send-tx.svg'
-import ContactsAdd from '../../../assets/icons/contacts-add.svg'
-import CopyToClipboard from '../../CopyToClipboard'
 import { pluralize } from '../../../util/pluralize'
-import classNames from 'classnames'
 
 type Props = {
   findContact: (address: string) => React$Node | null,
@@ -20,9 +17,8 @@ type Props = {
   amount: string | number,
   to: string,
   confirmations: number,
-  showAddContactModal: (address: string) => void,
+
   renderTxDate: (time: number) => React$Node | null,
-  intl: IntlShape,
 }
 
 class N3PendingAbstract extends React.Component<Props> {
@@ -33,54 +29,13 @@ class N3PendingAbstract extends React.Component<Props> {
       to,
       blocktime,
       findContact,
-      showAddContactModal,
       confirmations,
       renderTxDate,
-      intl,
     } = this.props
     const contactTo = findContact(to)
-    const contactToExists = contactTo !== to
     const logo = asset.image && (
       <img src={asset.image} alt={`${asset.symbol} logo`} />
     )
-
-    //   <div className={classNames(styles.transactionContainerN3)}>
-    //   <div className={styles.abstractContainerN3}>
-    //     <div className={styles.txTypeIconContainerN3}>
-    //       <div className={styles.sendIconContainer}>
-    //         <SendIcon />
-    //       </div>
-    //     </div>
-    //     {isPending ? 'Pending' : txDate}
-    //     <div className={styles.txLabelContainerN3}>Transfer</div>
-    //   </div>
-
-    //   <div className={styles.txToContainerN3}>
-    //     <div className={styles.txTransferContainerN3}>
-    //       <div className={styles.txTokenContainerN3}>
-    //         {logo}
-    //         {symbol}
-    //       </div>
-    //       <div className={styles.txAmountContainerN3}>{amount}</div>
-    //     </div>
-    //     <div className={styles.txSubjectContainerN3}>
-    //       <p>{contactTo}</p>
-    //       <CopyToClipboard
-    //         className={styles.copy}
-    //         text={contactTo}
-    //         tooltip={intl.formatMessage({ id: 'copyAddressTooltip' })}
-    //       />
-    //     </div>
-    //     <Button
-    //       className={styles.transactionHistoryButton}
-    //       renderIcon={ContactsAdd}
-    //       onClick={() => showAddContactModal(to)}
-    //       disabled={contactToExists}
-    //     >
-    //       <FormattedMessage id="activityAddAddress" />
-    //     </Button>
-    //   </div>
-    // </div>
 
     return (
       <Fragment>
