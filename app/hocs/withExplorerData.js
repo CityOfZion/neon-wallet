fc1071eef26dbc61e9fc8ff833681911698497f5
Maxwell Lasky <mlasky46@gmail.com>
2022-04-29 13:51:26 -0600

    Fix test

--- app/hocs/withExplorerData.js
+++ app/hocs/withExplorerData.js
@@ -2,8 +2,11 @@
 import { withData } from 'spunky'
 
 import settingsActions from '../actions/settingsActions'
+import { DEFAULT_EXPLORER } from '../core/constants'
 
 export default function withExplorerData(key: string = 'explorer') {
-  const mapSettingsDataToProps = settings => ({ [key]: settings.blockExplorer })
+  const mapSettingsDataToProps = settings => ({
+    [key]: settings.blockExplorer || DEFAULT_EXPLORER,
+  })
   return withData(settingsActions, mapSettingsDataToProps)
 }
