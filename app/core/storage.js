2120c3c5c17a7f86c2d210738dc1e3db1074bb79
Maxwell Lasky <mlasky46@gmail.com>
2022-08-12 08:26:02 -0600

    Replace all instances of dialog with bridge

--- app/core/storage.js
+++ app/core/storage.js
@@ -8,7 +8,7 @@ const set = promisify(storage.set, storage)
 const ENCRYPTED_FILES_WHITELIST = ['address']
 
 export const setStorage = async (key, value, encrypt = false) => {
-  const path = await ipcRenderer.invoke('getPath')
+  const path = await ipcRenderer.invoke('getStoragePath')
   storage.setDataPath(path)
   const encryptedValue = await ipcRenderer.invoke(
     'safeStorageEncrypt',
@@ -18,25 +18,22 @@ export const setStorage = async (key, value, encrypt = false) => {
 }
 
 export const getStorage = async key => {
-  const path = await ipcRenderer.invoke('getPath')
+  const path = await ipcRenderer.invoke('getStoragePath')
   storage.setDataPath(path)
   const value = await get(key)
-
+  const encryptionIsWhitelisted = !!ENCRYPTED_FILES_WHITELIST.find(fileName =>
+    key.toLowerCase().includes(fileName),
+  )
   // If the file name being requested includes address
   // and is NOT encrypted, we encrypt the file.
-  if (
-    key &&
-    !!ENCRYPTED_FILES_WHITELIST.find(fileName =>
-      key.toLowerCase().includes(fileName),
-    )
-  ) {
+  if (key && encryptionIsWhitelisted) {
     // if the value is a valid JS object it has not been encrypted
     if (typeof value === 'object') {
       await setStorage(key, value, true)
     }
   }
   // Only encrypted values get stored as strings
-  if (typeof value === 'string') {
+  if (typeof value === 'string' && encryptionIsWhitelisted) {
     const decryptedValue = await ipcRenderer.invoke('safeStorageDecrypt', value)
     return JSON.parse(decryptedValue)
   }
