e62cf7c233d844af28c668eea79213bf9b21cd9b
Maxwell Lasky <mlasky46@gmail.com>
2022-09-26 11:08:25 -0600

    feature: update wc constants (#2431)
    * Fix wallet connect
    
    * Revert logging
    
    * lint
    
    * Add back missing packages
    
    * Remove unused file
    
    * Bump version

--- app/components/Root/Root.jsx
+++ app/components/Root/Root.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 import { WalletConnectContextProvider } from '../../context/WalletConnect/WalletConnectContext'
+import {
+  DEFAULT_APP_METADATA,
+  DEFAULT_PROJECT_ID,
+  DEFAULT_RELAY_PROVIDER,
+} from '../../core/constants'
 
 import IntlWrapper from './IntlWrapper'
 import Routes from './Routes'
@@ -12,25 +17,9 @@ type Props = {
 }
 
 const wcOptions = {
-  chainIds: ['neo3:testnet', 'neo3:mainnet'],
-  methods: [
-    'invokeFunction',
-    'testInvoke',
-    'multiInvoke',
-    'multiTestInvoke',
-    'signMessage',
-    'verifyMessage',
-  ],
-  relayServer: 'wss://relay.walletconnect.org',
-  appMetadata: {
-    name: 'Neon Wallet',
-    description:
-      'An open source cross-platform light wallet for the NEO blockchain available on Windows, Mac OS, and Linux.',
-    url: 'https://coz.io/',
-    icons: [
-      'https://raw.githubusercontent.com/CityOfZion/visual-identity/develop/_CoZ%20Branding/_Logo/_Logo%20icon/_PNG%20200x178px/CoZ_Icon_DARKBLUE_200x178px.png',
-    ],
-  },
+  appMetadata: DEFAULT_APP_METADATA,
+  relayServer: DEFAULT_RELAY_PROVIDER,
+  projectId: DEFAULT_PROJECT_ID,
 }
 
 const Root = ({ store }: Props) => (
