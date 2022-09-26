b4c75274ddf6e2dd7a02a4cb33acbd7b46f70148
Maxwell Lasky <mlasky46@gmail.com>
2022-09-26 11:02:44 -0600

    Remove unused file

--- app/components/Root/Root.jsx
+++ app/components/Root/Root.jsx
@@ -2,9 +2,12 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
-import { DEFAULT_RELAY_PROVIDER } from '../../context/WalletConnect/constants'
 import { WalletConnectContextProvider } from '../../context/WalletConnect/WalletConnectContext'
-import { DEFAULT_APP_METADATA, DEFAULT_PROJECT_ID } from '../../core/constants'
+import {
+  DEFAULT_APP_METADATA,
+  DEFAULT_PROJECT_ID,
+  DEFAULT_RELAY_PROVIDER,
+} from '../../core/constants'
 
 import IntlWrapper from './IntlWrapper'
 import Routes from './Routes'
