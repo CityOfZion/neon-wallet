1d8295a388efb895c39f8d6f4c5e677be80b5e97
Raul Duarte Pereira <62621701+raulduartep@users.noreply.github.com>
2022-09-29 15:33:58 -0300

    CU-2pjkxye - No visible QR code (#2426)
    * CU-2pjkxye - No visible QR code
    
    * fix: CU-2pjkxye - No visible QR code
    
    Co-authored-by: LAPTOP-HBL4M4SL\rauld <raulduartepe@gmail.com.br>

--- app/components/Settings/EncryptSuccess/index.js
+++ app/components/Settings/EncryptSuccess/index.js
@@ -1,1 +1,27 @@
-export { default } from './EncryptSuccess'
+// @flow
+import { connect } from 'react-redux'
+import { compose } from 'recompose'
+import { bindActionCreators } from 'redux'
+import {
+  getEncryptedWIF,
+  resetEncryptedWIF,
+} from '../../../modules/generateEncryptedWIF'
+import EncryptSuccess from './EncryptSuccess'
+
+const mapStateToProps = (state: Object) => ({
+  encryptedWIF: getEncryptedWIF(state),
+})
+
+const actionCreators = {
+  resetEncryptedWIF,
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(actionCreators, dispatch)
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+)(EncryptSuccess)
