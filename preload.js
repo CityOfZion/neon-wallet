caa91e3df033bbcd890a18b3dc463641890eb6be
Kenedy Ribeiro <helplace2019@gmail.com>
2023-10-11 18:53:53 -0300

    CU-86a10fx0m - Implement Wcsdk's calculateFee

--- preload.js
+++ preload.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 const _process = process
+const crypto = require('crypto-browserify');
 process.once('loaded', function() {
   global.process = _process
   global.require = require
   global.Buffer = Buffer
+  global.crypto = crypto
 })
