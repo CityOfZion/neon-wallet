c3ef92454f9ba5a3ed6b3506ea64af56d4a38545
Ricardo Akio Kobayashi <ricardo.kobayashi@simpli.com.br>
2023-12-07 17:44:28 -0300

    86a1jgy3y - NEON3 - Implement Basic Activity Screen (without filters)

--- src/renderer/src/layouts/Portfolio.tsx
+++ src/renderer/src/layouts/Portfolio.tsx
@@ -1,3 +1,5 @@
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 import { MainLayout, TMainLayoutProps } from './Main'
 
 export const PortfolioLayout = ({ children, ...props }: TMainLayoutProps): JSX.Element => {
@@ -6,7 +8,27 @@ export const PortfolioLayout = ({ children, ...props }: TMainLayoutProps): JSX.E
       {children}
       <div className="flex-grow flex flex-col gap-y-5">
         <div className="w-full flex bg-gray-800 rounded flex-grow drop-shadow-lg animate-pulse"></div>
-        <div className="w-full flex bg-gray-800 rounded flex-grow drop-shadow-lg animate-pulse"></div>
+        <div className="w-full flex bg-gray-800 rounded flex-grow drop-shadow-lg animate-pulse">
+          <InfiniteScroll
+            dataLength={items.length} //This is important field to render the next data
+            next={fetchData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+            // below props only if you need pull down functionality
+            refreshFunction={this.refresh}
+            pullDownToRefresh
+            pullDownToRefreshThreshold={50}
+            pullDownToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>}
+            releaseToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>}
+          >
+            {items}
+          </InfiniteScroll>
+        </div>
       </div>
     </MainLayout>
   )
