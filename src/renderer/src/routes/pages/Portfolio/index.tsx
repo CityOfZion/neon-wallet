fe70c2ec6e4ddc4564864a70b21e639977dbc773
Ricardo Akio Kobayashi <ricardo.kobayashi@simpli.com.br>
2024-02-16 11:16:08 -0300

    CU-86a27h8r0 - NEON3 - Portfolio Screen/Wallet Screen - Token List

--- src/renderer/src/routes/pages/Portfolio/index.tsx
+++ src/renderer/src/routes/pages/Portfolio/index.tsx
@@ -1,15 +1,30 @@
 import { useTranslation } from 'react-i18next'
 import { TbRefresh } from 'react-icons/tb'
+import { AccountDataPanel } from '@renderer/components/AccountData/AccountDataPanel'
 import { IconButton } from '@renderer/components/IconButton'
-import { PortfolioLayout } from '@renderer/layouts/Portfolio'
+import { Separator } from '@renderer/components/Separator'
+import { useAccountsSelector } from '@renderer/hooks/useAccountSelector'
+import { useBalancesAndExchange } from '@renderer/hooks/useBalancesAndExchange'
+import { MainLayout } from '@renderer/layouts/Main'
 
 export const PortfolioPage = () => {
   const { t } = useTranslation('pages', { keyPrefix: 'portfolio' })
+  const { accounts } = useAccountsSelector()
+  const balanceExchange = useBalancesAndExchange(accounts)
 
   return (
-    <PortfolioLayout
+    <MainLayout
       heading={t('title')}
       rightComponent={<IconButton icon={<TbRefresh />} size="md" text={t('buttonRefreshLabel')} disabled />}
-    />
+    >
+      <div className="flex-grow flex flex-col gap-y-5">
+        <div className="w-full flex bg-gray-800 rounded flex-grow max-h-48"></div>
+        <div className="w-full flex flex-col bg-gray-800 rounded flex-grow p-4">
+          <h1 className="text-white pb-4">{t('allAccounts')}</h1>
+          <Separator />
+          <AccountDataPanel balanceExchange={balanceExchange} accounts={accounts} />
+        </div>
+      </div>
+    </MainLayout>
   )
 }
