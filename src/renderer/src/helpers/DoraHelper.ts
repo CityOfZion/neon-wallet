06997594775ed44d9d8136cb60c15a63b34eb62e
Ricardo Akio Kobayashi <ricardo.kobayashi@simpli.com.br>
2024-02-20 12:45:41 -0300

    86a27hnvt - NEON3 - Portfolio Screen/Wallet Screen - NFT List View

--- src/renderer/src/helpers/DoraHelper.ts
+++ src/renderer/src/helpers/DoraHelper.ts
@@ -1,5 +1,8 @@
 export class DoraHelper {
   static buildTransactionUrl = (hash: string): string => {
-    return 'https://dora.coz.io/transaction/neo3/mainnet/' + hash
+    return `https://dora.coz.io/transaction/neo3/mainnet/${hash}`
+  }
+  static buildNftUrl = (hash: string, id: string): string => {
+    return `https://dora.coz.io/nft/neo3/mainnet/${hash}/${id}`
   }
 }
